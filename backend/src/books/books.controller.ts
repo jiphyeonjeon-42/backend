cefb731eb068e6d2f986929607e6fc6692802a81
YeonSeong-Lee <dltjddus3@gmail.com>
2022-05-13 16:54:38 +0900

    JIP-28 신규도서 swagger 설정

--- backend/src/books/books.controller.ts
+++ backend/src/books/books.controller.ts
@@ -1,43 +1,52 @@
-import {
-  NextFunction, Request, RequestHandler, Response,
-} from 'express';
-import * as status from 'http-status';
-import ErrorResponse from '../errorResponse';
-import * as BooksService from './books.service';
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import * as status from "http-status";
+import ErrorResponse from "../errorResponse";
+import * as BooksService from "./books.service";
 
 export interface SearchBookInfoQuery {
-  query: string,
-  sort: string,
-  page: string,
-  limit: string,
-  category: string,
+  query: string;
+  sort: string;
+  page: string;
+  limit: string;
+  category: string;
 }
 
 export const searchBookInfo = async (
   req: Request<{}, {}, {}, SearchBookInfoQuery>,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) => {
-  const {
-    query, sort, page, limit, category,
-  } = req.query;
+  const { query, sort, page, limit, category } = req.query;
   if (!(query && page && limit)) {
-    next(new ErrorResponse(status.BAD_REQUEST, 'query, page, limit 중 하나 이상이 없습니다.'));
-  } else {
-    res.status(status.OK).json(
-      await BooksService.searchInfo(query, sort, parseInt(page, 10), parseInt(limit, 10), category),
+    next(
+      new ErrorResponse(
+        status.BAD_REQUEST,
+        "query, page, limit 중 하나 이상이 없습니다."
+      )
     );
+  } else {
+    res
+      .status(status.OK)
+      .json(
+        await BooksService.searchInfo(
+          query,
+          sort,
+          parseInt(page, 10),
+          parseInt(limit, 10),
+          category
+        )
+      );
   }
 };
 
 export const getInfoId: RequestHandler = async (
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) => {
   const bookId = parseInt(req.params.id, 10);
   if (Number.isNaN(bookId)) {
-    next(new ErrorResponse(status.BAD_REQUEST, 'id가 숫자가 아닙니다.'));
+    next(new ErrorResponse(status.BAD_REQUEST, "id가 숫자가 아닙니다."));
   } else {
     res.status(status.OK).json(await BooksService.getInfo(bookId));
   }
@@ -45,10 +54,10 @@ export const getInfoId: RequestHandler = async (
 export const info: RequestHandler = (req: Request, res: Response) => {};
 
 export const booker: RequestHandler = (req: Request, res: Response) => {
-  res.send('hello express');
+  res.send("hello express");
   // search 함수
 };
 
 export const search: RequestHandler = (req: Request, res: Response) => {
-  res.send('hello express');
+  res.send("hello express");
 };
