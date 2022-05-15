f9b634dd673ea768291e428f6df5b4632b817a9c
YeonSeong-Lee <dltjddus3@gmail.com>
2022-05-15 19:19:07 +0900

    JIP-28 FEATURE: 신규도서가 정렬의 기본값으로 변경

--- backend/src/books/books.service.ts
+++ backend/src/books/books.service.ts
@@ -1,69 +1,73 @@
-import { RowDataPacket } from 'mysql2';
-import { executeQuery } from '../mysql';
-import { StringRows } from '../utils/types';
+import { RowDataPacket } from "mysql2";
+import { executeQuery } from "../mysql";
+import { StringRows } from "../utils/types";
 
 export interface BookInfo extends RowDataPacket {
-  id?: number,
-  title: string,
-  author: string,
-  publisher: string,
-  isbn?: string
-  image: string,
-  category: string,
-  publishedAt?: string | Date,
-  createdAt: Date,
-  updatedAt: Date
+  id?: number;
+  title: string;
+  author: string;
+  publisher: string;
+  isbn?: string;
+  image: string;
+  category: string;
+  publishedAt?: string | Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface BookEach extends RowDataPacket {
-  id?: number,
-  donator: string,
-  donatorId?: number,
-  callSign: string,
-  status: number,
-  createdAt: Date,
-  updatedAt: Date,
-  infoId: number,
+  id?: number;
+  donator: string;
+  donatorId?: number;
+  callSign: string;
+  status: number;
+  createdAt: Date;
+  updatedAt: Date;
+  infoId: number;
 }
 
 export interface Book {
-  title: string,
-  author: string,
-  publisher: string,
-  isbn: string
-  image?: string,
-  category: string,
-  publishedAt?: Date,
-  donator?: string,
-  callSign: string,
-  status: number,
+  title: string;
+  author: string;
+  publisher: string;
+  isbn: string;
+  image?: string;
+  category: string;
+  publishedAt?: Date;
+  donator?: string;
+  callSign: string;
+  status: number;
 }
 
 interface categoryCount extends RowDataPacket {
-  name: string,
-  count: number,
+  name: string;
+  count: number;
 }
 
 interface lending extends RowDataPacket {
-  lendingCreatedAt: Date,
-  returningCreatedAt: Date,
+  lendingCreatedAt: Date;
+  returningCreatedAt: Date;
 }
 
 export const createBook = async (book: Book): Promise<void> => {
-  const result = (await executeQuery(`
+  const result = (await executeQuery(
+    `
     SELECT
       isbn
     FROM book_info
     WHERE isbn = ?
-  `, [book.isbn])) as StringRows[];
+  `,
+    [book.isbn]
+  )) as StringRows[];
   if (result.length === 0) {
     let image = null;
     if (!book.image) {
       image = `https://image.kyobobook.co.kr/images/book/xlarge/${book.isbn.slice(
-        -3,
+        -3
       )}/x${book.isbn}.jpg`;
     }
-    await executeQuery(`
+    await executeQuery(
+      `
     INSERT INTO book_info(
       title,
       author,
@@ -85,17 +89,20 @@ export const createBook = async (book: Book): Promise<void> => {
         WHERE name = ?
       ),
       ?
-    )`, [
-      book.title,
-      book.author,
-      book.publisher,
-      book.isbn,
-      book.image ?? image,
-      book.category,
-      book.publishedAt,
-    ]);
+    )`,
+      [
+        book.title,
+        book.author,
+        book.publisher,
+        book.isbn,
+        book.image ?? image,
+        book.category,
+        book.publishedAt,
+      ]
+    );
   }
-  await executeQuery(`
+  await executeQuery(
+    `
     INSERT INTO book(
       donator,
       donatorId,
@@ -117,33 +124,44 @@ export const createBook = async (book: Book): Promise<void> => {
         WHERE isbn = ?
       )
     )
-  `, [
-    book.donator ?? 'null',
-    book.donator ?? '0',
-    book.callSign,
-    book.status,
-    book.isbn,
-  ]);
+  `,
+    [
+      book.donator ?? "null",
+      book.donator ?? "0",
+      book.callSign,
+      book.status,
+      book.isbn,
+    ]
+  );
 };
 
 export const deleteBook = async (book: Book): Promise<boolean> => {
-  const result = (await executeQuery(`
+  const result = (await executeQuery(
+    `
     SELECT *
     FROM book
     WHERE callSign = ?
-  `, [book.callSign])) as BookEach[];
+  `,
+    [book.callSign]
+  )) as BookEach[];
   if (result.length === 0) {
     return false;
   }
-  await executeQuery(`
+  await executeQuery(
+    `
     DELETE FROM book
     WHERE callSign = ?
-  `, [book.callSign]);
+  `,
+    [book.callSign]
+  );
   if (result.length === 1) {
-    await executeQuery(`
+    await executeQuery(
+      `
       DELETE FROM book_info
       WHERE id = ?
-    `, [result[0].infoId]);
+    `,
+      [result[0].infoId]
+    );
   }
   return true;
 };
@@ -153,30 +171,33 @@ export const searchInfo = async (
   sort: string,
   page: number,
   limit: number,
-  category: string | null,
+  category: string | null
 ) => {
-  let ordering = '';
+  let ordering = "";
   switch (sort) {
-    case 'title':
-      ordering = 'ORDER BY book_info.title';
+    case "title":
+      ordering = "ORDER BY book_info.title";
       break;
-      // case 'popular':
-      //   ordering = 'ORDER BY count(lending.id) DESC';
-      // break;
-    case 'new':
-      ordering = 'ORDER BY book_info.publishedAt DESC';
-      break;
+    // case 'popular':
+    //   ordering = 'ORDER BY count(lending.id) DESC';
+    // break
     default:
-      ordering = 'ORDER BY book_info.createdAt DESC';
+      ordering = "ORDER BY book_info.createdAt DESC";
   }
-  const categoryResult = await executeQuery(`
+  const categoryResult = (await executeQuery(
+    `
     SELECT name
     FROM category
     WHERE name = ?
-  `, [category]) as StringRows[];
+  `,
+    [category]
+  )) as StringRows[];
   const categoryName = categoryResult?.[0]?.name;
-  const categoryWhere = categoryName ? `category.name = '${categoryName}'` : 'TRUE';
-  const categoryList = await executeQuery(`
+  const categoryWhere = categoryName
+    ? `category.name = '${categoryName}'`
+    : "TRUE";
+  const categoryList = (await executeQuery(
+    `
     SELECT
       category.name AS name,
       count(name) AS count
@@ -190,12 +211,12 @@ export const searchInfo = async (
         ${categoryWhere}
       )
     GROUP BY name;
-  `, [`%${query}%`,
-    `%${query}%`,
-    `%${query}%`,
-  ]) as categoryCount[];
-  const categoryHaving = categoryName ? `category = '${categoryName}'` : 'TRUE';
-  const bookList = await executeQuery(`
+  `,
+    [`%${query}%`, `%${query}%`, `%${query}%`]
+  )) as categoryCount[];
+  const categoryHaving = categoryName ? `category = '${categoryName}'` : "TRUE";
+  const bookList = (await executeQuery(
+    `
     SELECT
       book_info.id AS id,
       book_info.title AS title,
@@ -221,12 +242,9 @@ export const searchInfo = async (
     ${ordering}
     LIMIT ?
     OFFSET ?;
-  `, [`%${query}%`,
-    `%${query}%`,
-    `%${query}%`,
-    limit,
-    page * limit,
-  ]) as BookInfo[];
+  `,
+    [`%${query}%`, `%${query}%`, `%${query}%`, limit, page * limit]
+  )) as BookInfo[];
 
   const totalItems = categoryList.reduce((prev, curr) => prev + curr.count, 0);
   const meta = {
@@ -241,28 +259,32 @@ export const searchInfo = async (
 
 const statusConverter = (status: number, dueDate: string) => {
   if (status === 0) {
-    if (dueDate !== '-') return '대출 중';
-    return '비치 중';
-  } if (status === 1) return '분실';
-  if (status === 2) return '파손';
-  return '알 수 없음';
+    if (dueDate !== "-") return "대출 중";
+    return "비치 중";
+  }
+  if (status === 1) return "분실";
+  if (status === 2) return "파손";
+  return "알 수 없음";
 };
 
 const getDueDate = (lendingData: lending[]) => {
-  if (lendingData && lendingData.length === 0) return '-';
+  if (lendingData && lendingData.length === 0) return "-";
   const lastLending = lendingData.sort(
-    (a, b) => new Date(b.lendingCreatedAt).getTime() - new Date(a.lendingCreatedAt).getTime(),
+    (a, b) =>
+      new Date(b.lendingCreatedAt).getTime() -
+      new Date(a.lendingCreatedAt).getTime()
   )[0];
   if (lastLending.returningCreatedAt) {
-    return '-';
+    return "-";
   }
   const tDate = new Date(lastLending.lendingCreatedAt);
   tDate.setDate(tDate.getDate() + 14);
-  return tDate.toJSON().substring(2, 10).split('-').join('.');
+  return tDate.toJSON().substring(2, 10).split("-").join(".");
 };
 
 export const getInfo = async (id: number) => {
-  const [bookSpec] = await executeQuery(`
+  const [bookSpec] = (await executeQuery(
+    `
     SELECT
       id,
       title,
@@ -279,13 +301,16 @@ export const getInfo = async (id: number) => {
     FROM book_info
     WHERE
       id = ?
-  `, [id]) as BookInfo[];
+  `,
+    [id]
+  )) as BookInfo[];
   if (bookSpec.publishedAt) {
     const date = new Date(bookSpec.publishedAt);
     bookSpec.publishedAt = `${date.getFullYear()}년 ${date.getMonth() + 1}월`;
   }
 
-  const eachBook = await executeQuery(`
+  const eachBook = (await executeQuery(
+    `
     SELECT
       id,
       callSign,
@@ -294,11 +319,15 @@ export const getInfo = async (id: number) => {
     FROM book
     WHERE
       infoId = ?
-  `, [id]) as BookEach[];
+  `,
+    [id]
+  )) as BookEach[];
 
   const donators: string[] = [];
-  const books = await Promise.all(eachBook.map(async (val) => {
-    const lendingData = await executeQuery(`
+  const books = await Promise.all(
+    eachBook.map(async (val) => {
+      const lendingData = (await executeQuery(
+        `
       SELECT
         lending.createdAt AS lendingCreatdAt,
         returning.createdAt AS returningCreatedAt
@@ -306,20 +335,23 @@ export const getInfo = async (id: number) => {
       LEFT JOIN returning ON lending.id = returning.lendingId
       WHERE
         bookId = ?
-    `, [val.id]) as lending[];
-    const dueDate = getDueDate(lendingData);
-    const status = statusConverter(val.status, dueDate);
+    `,
+        [val.id]
+      )) as lending[];
+      const dueDate = getDueDate(lendingData);
+      const status = statusConverter(val.status, dueDate);
 
-    if (val.donator) {
-      donators.push(val.donator);
-    }
-    const { donator, ...rest } = val;
-    return { ...rest, dueDate, status };
-  }));
+      if (val.donator) {
+        donators.push(val.donator);
+      }
+      const { donator, ...rest } = val;
+      return { ...rest, dueDate, status };
+    })
+  );
   if (donators.length === 0) {
-    bookSpec.donators = '-';
+    bookSpec.donators = "-";
   } else {
-    bookSpec.donators = donators.join(', ');
+    bookSpec.donators = donators.join(", ");
   }
   bookSpec.books = books;
   return bookSpec;
