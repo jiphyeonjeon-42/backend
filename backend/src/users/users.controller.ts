1cafa481f3f3843c6fb0cddae02bb680548a1fb1
keinn51 <soogood97@gmail.com>
2022-05-16 19:59:07 +0900

    JIP-22 controller search 변경

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,19 +1,23 @@
 import { Request, RequestHandler, Response } from 'express';
-import { searchAllUsers, searchUserByIntraId } from './users.service';
+import { searchAllUsers, searchUserByNickName } from './users.service';
 
-interface Params {
-  intraId: string;
+export interface searchQuery {
+  nickName: string;
   page?: number;
   limit?: number;
 }
 
-export const search: RequestHandler = async (req: Request, res: Response) => {
-  const { intraId, page = 1, limit = 5 }: Params = req.body;
-  if (intraId === '') {
+export const search = async (
+  req: Request<{}, {}, {}, searchQuery>,
+  res: Response,
+) => {
+  const { nickName = '', page = 1, limit = 5 } = req.query;
+  if (nickName === '') {
     res.send(searchAllUsers(page, limit));
+  } else if (nickName) {
+    const items = JSON.parse(JSON.stringify(await searchUserByNickName(nickName, limit, page)));
+    res.send(items);
   }
-  const items = JSON.parse(JSON.stringify(await searchUserByIntraId(intraId, limit, page)));
-  res.send(items);
 };
 
-export const create: RequestHandler = (req: Request, res: Response) => {};
+export const create = (req: Request, res: Response) => {};
