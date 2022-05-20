fdf7c3b106df532bb8a5982036cbb7982740a6d6
Oracle Public Cloud User <opc@instance-1.subnet12310950.vcn12310950.oraclevcn.com>
2022-05-20 04:06:02 +0000

    JIP-37 CHORE: 사용하지 않을 SQL 파일 삭제

--- database/03_add_columns_to_users_table.sql
+++
@@ -1,8 +0,0 @@
-# user 테이블 수정
-alter table user change login email varchar(255) NOT NULL after id;
-alter table user add passwd varchar(255) NOT NULL after email;
-alter table user change intra intraId int NULL;
-alter table user add intraKey int NULL after intraId;
-alter table user change slack slack varchar(255) NULL;
-alter table user change librarian role int NOT NULL default 0 after penaltyAt;
-alter table user change penaltyAt penaltyEndDay datetime NULL;
