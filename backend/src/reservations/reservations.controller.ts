ba9487279a58282f2843acf7587088038b4fe8f7
sujikim <sujikim@student.42seoul.kr>
2022-05-22 21:45:40 +0900

    JIP-66 FEATURE: 예약 search 추가

--- backend/src/reservations/reservations.controller.ts
+++ backend/src/reservations/reservations.controller.ts
@@ -1,16 +1,29 @@
 import { Request, RequestHandler, Response } from 'express';
 import * as reservationsService from './reservations.service';
-import { ReservationsPageInfo } from '../paginate';
 
 export const create: RequestHandler = (req: Request, res: Response) => {};
 
+const filterCheck = (argument: string) => {
+  switch (argument) {
+    case 'pending':
+    case 'waiting':
+    case 'expired':
+    case 'all':
+      return 1;
+    default:
+      return 0;
+  }
+};
+
 export const search: RequestHandler = async (req: Request, res: Response) => {
+  // 사서 권한 체크 필요
   const info = req.query;
-  const page = info.page as string ? info.page as string : '1';
-  const limit = info.limit as string ? info.limit as string : '5';
-  const filter = info.filter as string[];
-  const p :ReservationsPageInfo = new ReservationsPageInfo(page, limit, filter);
-  const data = await reservationsService
-    .searchReservation(p.getPage(), p.getLimit(), p.getFilter());
-  res.send(data);
+  const query = info.query as string ? info.query as string : '';
+  const page = parseInt(info.page as string, 10) ? parseInt(info.page as string, 10) - 1 : 0;
+  const limit = parseInt(info.limit as string, 10) ? parseInt(info.limit as string, 10) : 5;
+  const filter = info.filter as string;
+  if (!filterCheck) { res.status(400); }
+  const result = await reservationsService
+    .searchReservation(query, page, limit, filter);
+  res.send(result);
 };
--- backend/src/reservations/reservations.service.ts
+++ backend/src/reservations/reservations.service.ts
@@ -1,27 +1,70 @@
-import { pool } from '../mysql';
-import { paginate } from '../paginate';
+import { executeQuery } from '../mysql';
+import * as models from '../users/users.model';
 
-export const searchReservation = async (page: string, limit: string, filter: string[]) => {
-    console.log('page: ' + page);
-    console.log('limit: ' + limit);
-    console.log('filter: ' + filter);
-    if (!filter) filter = []; //query에 filter가 없을 시 나타나는 에러 방지
-    if (!filter.includes('proceeding') && !filter.includes('finish')){ //둘다 선택 x
-        console.log('둘다 선택 x');
+export const
+  searchReservation = async (query:string, page: number, limit: number, filter: string) => {
+    let filterQuery;
+    switch (filter) {
+      case 'waiting':
+        filterQuery = 'WHERE reservation.status = 0 AND reservation.bookId IS NULL';
+        break;
+      case 'expired':
+        filterQuery = 'WHERE reservation.status > 0';
+        break;
+      case 'all':
+        filterQuery = '';
+        break;
+      default:
+        filterQuery = 'WHERE reservation.status = 0 AND reservation.bookId IS NOT NULL';
     }
-    if (filter.includes('proceeding') && filter.includes('finish')){ //둘다 선택 x
-        console.log('둘다 선택 O');
-    }
-    else if (filter.includes('proceeding')) //proceeding
-    {
-        console.log('proceeding');
-    }
-    else if (filter.includes('finish')) //finish
-    {
-        console.log('finish');        
-    }
-    const data = await pool.query( `SELECT * FROM book`);
-    const item = JSON["parse"](JSON.stringify(data[0]));
-    console.log(paginate(item, 150, parseInt(page)));
-    return paginate(item, 150, parseInt(page));
-}
+
+    const items = (await executeQuery(`
+      SELECT 
+        reservation.id AS reservationsId,
+        user.nickName AS login,
+        DATEDIFF(now(), user.penaltyEndDay) AS penaltyDays,
+        book.title,
+        book.image,
+        (
+          SELECT callSign 
+          FROM book
+          WHERE book.id = reservation.bookId
+        ) AS callSign,
+        reservation.createdAt,
+        endAt,
+        status
+      FROM reservation
+      LEFT JOIN user AS user ON reservation.userId = user.id
+      LEFT JOIN book_info AS book ON reservation.bookInfoId = book.id
+      ${filterQuery}
+      HAVING book.title LIKE ? OR login LIKE ? OR callSign LIKE ?
+      LIMIT ?
+      OFFSET ?
+  `, [`%${query}%`, `%${query}%`, `%${query}%`, limit, limit * page]));
+    const totalItems = (await executeQuery(`
+      SELECT 
+        reservation.id AS reservationsId,
+        user.nickName AS login,
+        book.title,
+        (
+          SELECT callSign 
+          FROM book
+          WHERE book.id = reservation.bookId
+        ) AS callSign
+      FROM reservation
+      LEFT JOIN user AS user ON reservation.userId = user.id
+      LEFT JOIN book_info AS book ON reservation.bookInfoId = book.id
+      ${filterQuery}
+      HAVING book.title LIKE '%a%' OR login LIKE '%a%' OR callSign LIKE '%a%'
+    `, [`%${query}%`, `%${query}%`, `%${query}%`]));
+    const meta :models.Meta = {
+      totalItems: totalItems.length,
+      itemCount: items.length,
+      itemsPerPage: limit,
+      totalPages: Math.ceil(totalItems.length / limit),
+      currentPage: page + 1,
+    };
+    return { items, meta };
+  };
+
+export const a = 1;
