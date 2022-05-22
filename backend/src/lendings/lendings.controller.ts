5c38b6dc6fe589a4370ed2814cf5ff6392620620
Jiwon-Woo <jwoo@student.42seoul.kr>
2022-05-23 02:59:12 +0900

    JIP-78 FEATURE: 반납처리하는 lending return api 추가

--- backend/src/lendings/lendings.controller.ts
+++ backend/src/lendings/lendings.controller.ts
@@ -43,4 +43,23 @@ export const booksId: RequestHandler = (req: Request, res: Response) => {
 
 export const returnBook: RequestHandler = (req: Request, res: Response) => {
   res.send('hello express');
+  if (req.query.role < 3) { res.status(401); }
+  if (!req.body.lendingId || req.body.condition) {
+    res.status(401).json({ errorCode: 1 });
+  }
+  const result = await lendingsService.returnBook(
+    req.query.id,
+    req.body.lendingId,
+    req.body.condition,
+  );
+
+  if (result === lendingsService.ok) {
+    res.status(status.OK);
+  } else if (result === lendingsService.nonexistentLending) {
+    res.status(400).json({ errorCode: 2 });
+  } else if (result === lendingsService.alreadyReturned) {
+    res.status(400).json({ errorCode: 3 });
+  } else {
+    res.status(500);
+  }
 };
--- backend/src/lendings/lendings.service.ts
+++ backend/src/lendings/lendings.service.ts
@@ -8,6 +8,8 @@ export const onReservation = '예약된 책';
 export const lostBook = '분실';
 export const damagedBook = '파손';
 export const ok = 'ok';
+export const nonexistentLending = '존재하지 않는 대출';
+export const alreadyReturned = '이미 반납 처리된 대출';
 
 export const create = async (
   userId: number,
@@ -83,3 +85,65 @@ export const create = async (
 };
 
 export const search = async () => {};
+
+export const returnBook = async (
+  librarianId: number,
+  lendingId: number,
+  condition: string,
+): Promise<string> => {
+  let message = ok;
+  const conn = await pool.getConnection();
+  const transactionExecuteQuery = makeExecuteQuery(conn);
+  try {
+    await conn.beginTransaction();
+
+    // 유효한 lendingId 인지, 반납처리가 이미 된 책인지 확인
+    const lendingInfo = await transactionExecuteQuery(`
+      SELECT *
+      FROM lending
+      WHERE id = ?;
+    `, [lendingId]);
+    if (!lendingInfo || !lendingInfo[0]) {
+      throw new Error(nonexistentLending);
+    } else if (lendingInfo[0].returnedAt) {
+      throw new Error(alreadyReturned);
+    }
+
+    await transactionExecuteQuery(`
+      UPDATE lending
+      SET returningLibrarian = ?,
+          returningCondition = ?
+      WHERE id = ?
+    `, [librarianId, lendingId, condition]);
+
+    // 예약된 책이 있다면 예약 부여, endAt 어떻게 처리하지..?
+    const isReserved = await transactionExecuteQuery(`
+      SELECT *
+      FROM reservation
+      WHERE bookInfoId = (
+        SELECT infoId
+        FROM book
+        WHERE id = ?
+      ) AND status = 0 AND bookId IS NULL
+      ORDER BY createdAt
+      LIMIT 1;
+    `, [lendingInfo[0].bookId]);
+    if (isReserved && isReserved[0]) {
+      await transactionExecuteQuery(`
+        UPDATE reservation
+        SET bookId = ?
+        WHERE id = ?
+    `, [lendingInfo[0].bookId, isReserved[0].id]);
+    }
+
+    await conn.commit();
+  } catch (e) {
+    await conn.rollback();
+    if (e instanceof Error) {
+      message = e.message;
+    }
+  } finally {
+    conn.release();
+  }
+  return message;
+};
