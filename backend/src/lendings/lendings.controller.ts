d120ab47ae76b1d7d01e1cae64c7d4d1f4eff030
sujikim <sujikim@student.42seoul.kr>
2022-05-23 01:59:04 +0900

    JIP-74 FEATURE: lending search 추가

--- backend/src/lendings/lendings.controller.ts
+++ backend/src/lendings/lendings.controller.ts
@@ -1,13 +1,30 @@
 import { Request, RequestHandler, Response } from 'express';
+import * as status from 'http-status';
+import * as lendingsService from './lendings.service';
 
 export const create: RequestHandler = (req: Request, res: Response) => {
   res.send('hello express');
 };
 
-export const search: RequestHandler = (req: Request, res: Response) => {
-  res.send('hello express');
-  // search 함수
+const argumentCheck = (sort:string, type:string) => {
+  if (sort !== 'new' && sort !== 'old') { return 0; }
+  if (type !== 'user' && type !== 'title' && type !== 'callSign' && type !== 'all') { return 0; }
+  return 1;
 };
+
+export const search: RequestHandler = async (req: Request, res: Response) => {
+  // 사서 권한 확인 필요
+  const info = req.query;
+  const query = info.query as string;
+  const page = parseInt(info.page as string, 10) ? parseInt(info.page as string, 10) - 1 : 0;
+  const limit = parseInt(info.limit as string, 10) ? parseInt(info.limit as string, 10) : 5;
+  const sort = info.sort as string;
+  const type = info.type as string ? info.type as string : 'all';
+  if (!argumentCheck(sort, type)) res.status(400);
+  const result = await lendingsService.search(query, page, limit, sort, type);
+  res.send({ items: result });
+};
+
 export const booksId: RequestHandler = (req: Request, res: Response) => {
   res.send('hello express');
   // search 함수
--- backend/src/lendings/lendings.service.ts
+++ backend/src/lendings/lendings.service.ts
@@ -1,1 +1,47 @@
-//search할때 사용하는 sql문들
+import { executeQuery } from '../mysql';
+
+export const
+  search = async (query: string, page: number, limit: number, sort:string, type: string) => {
+    let filterQuery;
+    switch (type) {
+      case 'user':
+        filterQuery = `HAVING login LIKE '%${query}%'`;
+        break;
+      case 'title':
+        filterQuery = `HAVING title LIKE '%${query}%'`;
+        break;
+      case 'callSign':
+        filterQuery = `HAVING callSign LIKE '%UPPER(${query})%'`;
+        break;
+      default:
+        filterQuery = `HAVING login LIKE '%${query}%' OR title LIKE '%${query}%' OR callSign LIKE '%${query}%'`;
+    }
+    const orderQuery = sort === 'new' ? 'DESC' : '';
+    const result = await executeQuery(`
+    SELECT
+      lending.id,
+      lendingCondition,
+      user.nickname as login, 
+      CASE WHEN NOW() > user.penaltyEndDay THEN 0
+        ELSE DATEDIFF(now(), user.penaltyEndDay) 
+      END AS penaltyDays,
+      (
+          SELECT callSign
+          FROM book
+          WHERE id = bookId
+      ) as callSign,
+      (
+        SELECT title
+        FROM book_info
+        WHERE id = bookId
+      ) AS title,
+      DATE_FORMAT(DATE_ADD(lending.createdAt, interval 14 day), '%Y.%m.%d') AS dueDate
+    FROM lending
+    JOIN user AS user ON lending.userId = user.id
+    ${filterQuery}
+    ORDER BY lending.createdAt ${orderQuery}
+  `);
+    return result;
+  };
+
+export const a = 1;
