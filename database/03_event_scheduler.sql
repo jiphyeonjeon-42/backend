8716a2d8cc0c0d1bac7ccbe1cdfa2a08ae3a5f00
seunam <seunam@student.42seoul.kr>
2022-05-23 18:15:39 +0900

    JIP-81 FEATURE: add event_scheduler - update reservation table

---
+++ database/03_event_scheduler.sql
@@ -0,0 +1,6 @@
+CREATE EVENT update_reservation_status
+ON SCHEDULE EVERY 1 DAY
+STARTS '2022-05-24 00:00:00' ON COMPLETION PRESERVE ENABLE
+DO UPDATE reservation set status = 3 where (status = 0 and endAt <= NOW());
+
+SET GLOBAL event_scheduler = ON
