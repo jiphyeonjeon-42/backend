4e68db29531b0f28755cb93ac983e0a99f6ca343
keinn51 <soogood97@gmail.com>
2022-05-24 14:59:39 +0900

    JIP-88 기존 jest code 업데이트 된 dup data에 맞게 수정

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,5 +1,4 @@
 /* eslint no-console: "off" */
-import { FtTypes } from '../auth/auth.service';
 import { pool } from '../mysql';
 import * as UsersService from './users.service';
 
@@ -9,20 +8,20 @@ describe('UsersService', () => {
   });
 
   // searchUserByNickName
-  it('[searchUserByNickName] User jimin is', async () => {
-    expect(await UsersService.searchUserByNickName('jimin', 5, 0)).toStrictEqual({
+  it('[searchUserByNickName] User hihi is', async () => {
+    expect(await UsersService.searchUserByNickName('hihi', 5, 0)).toStrictEqual({
       items: [
         {
-          id: 14,
-          email: '',
-          password: '',
-          nickName: 'jimin',
-          intraId: 99994,
-          slack: 'U02LNNDRC9F',
-          penaltyEndDay: new Date(Date.parse('2021-12-07 20:06:18')),
+          id: 1414,
+          email: 'example_role1_7@gmail.com',
+          password: '4444',
+          nickname: 'hihi',
+          intraId: 44,
+          slack: 'dasdwqwe1132',
+          penaltyEndDay: new Date(Date.parse('2022-05-20 07:02:34')),
           role: 1,
-          createdAt: new Date(Date.parse('2021-12-07 20:06:18.991237')),
-          updatedAt: new Date(Date.parse('2021-12-07 20:06:18.991237')),
+          createdAt: new Date(Date.parse('2022-05-20 07:02:34.973193')),
+          updatedAt: new Date(Date.parse('2022-05-20 16:13:39.314069')),
         },
       ],
       meta: {
@@ -36,114 +35,95 @@ describe('UsersService', () => {
   });
 
   // searchUserById
-  it('[searchUserById] User id 14 (jimin) is', async () => {
-    expect(await UsersService.searchUserById(14)).toStrictEqual(
-      [{
-        id: 14,
-        email: '',
-        password: '',
-        nickName: 'jimin',
-        intraId: 99994,
-        slack: 'U02LNNDRC9F',
-        penaltyEndDay: new Date(Date.parse('2021-12-07 20:06:18')),
-        role: 1,
-        createdAt: new Date(Date.parse('2021-12-07 20:06:18.991237')),
-        updatedAt: new Date(Date.parse('2021-12-07 20:06:18.991237')),
-      }],
+  it('[searchUserById] User id 1414 (hihi) is', async () => {
+    expect(await UsersService.searchUserById(1414)).toStrictEqual(
+      {
+        items: [
+          {
+            id: 1414,
+            email: 'example_role1_7@gmail.com',
+            password: '4444',
+            nickname: 'hihi',
+            intraId: 44,
+            slack: 'dasdwqwe1132',
+            penaltyEndDay: new Date(Date.parse('2022-05-20 07:02:34')),
+            role: 1,
+            createdAt: new Date(Date.parse('2022-05-20 07:02:34.973193')),
+            updatedAt: new Date(Date.parse('2022-05-20 16:13:39.314069')),
+          },
+        ],
+      },
     );
   });
 
   // searchUserByIntraId
-  it('[searchUserByIntraId] User intraId 99994 (jimin) is', async () => {
-    expect(await UsersService.searchUserByIntraId(99994)).toStrictEqual(
-      [{
-        id: 14,
-        email: '',
-        password: '',
-        nickName: 'jimin',
-        intraId: 99994,
-        slack: 'U02LNNDRC9F',
-        penaltyEndDay: new Date(Date.parse('2021-12-07 20:06:18')),
-        role: 1,
-        createdAt: new Date(Date.parse('2021-12-07 20:06:18.991237')),
-        updatedAt: new Date(Date.parse('2021-12-07 20:06:18.991237')),
-      }],
+  it('[searchUserByIntraId] User intraId 44 (hihi) is', async () => {
+    expect(await UsersService.searchUserByIntraId(44)).toStrictEqual(
+      [
+        {
+          id: 1414,
+          email: 'example_role1_7@gmail.com',
+          password: '4444',
+          nickname: 'hihi',
+          intraId: 44,
+          slack: 'dasdwqwe1132',
+          penaltyEndDay: new Date(Date.parse('2022-05-20 07:02:34')),
+          role: 1,
+          createdAt: new Date(Date.parse('2022-05-20 07:02:34.973193')),
+          updatedAt: new Date(Date.parse('2022-05-20 16:13:39.314069')),
+        },
+      ],
     );
   });
 
   // searchAllUsers
-  it('[searchAllUsers] 5 Users in page 1 are', async () => {
-    expect(await UsersService.searchAllUsers(5, 1)).toStrictEqual({
+  it('[searchAllUsers] 3 Users in page 6 are', async () => {
+    expect(await UsersService.searchAllUsers(3, 6)).toStrictEqual({
       items: [
         {
-          id: 6,
-          email: '',
-          password: '',
-          nickName: 'donghyun',
-          intraId: 100015,
-          slack: 'U02LCAZBTL4',
-          penaltyEndDay: new Date(Date.parse('2021-12-07 20:06:18')),
-          role: 0,
-          createdAt: new Date(Date.parse('2021-12-07 20:06:18.973689')),
-          updatedAt: new Date(Date.parse('2021-12-07 20:06:18.973689')),
+          id: 1410,
+          email: 'example_role_1_3@gmail.com',
+          password: '1234',
+          nickname: 'seongyle3',
+          intraId: 3,
+          slack: null,
+          penaltyEndDay: new Date(Date.parse('2022-05-20 07:02:34')),
+          role: 1,
+          createdAt: new Date(Date.parse('2022-05-20 07:02:34.973193')),
+          updatedAt: new Date(Date.parse('2022-05-20 07:02:34.973193')),
         },
         {
-          id: 7,
-          email: '',
-          password: '',
-          nickName: 'ymoon',
-          intraId: 100013,
-          slack: 'U02LCAYAFCL',
-          penaltyEndDay: new Date(Date.parse('2021-12-07 20:06:18')),
-          role: 0,
-          createdAt: new Date(Date.parse('2021-12-07 20:06:18.975733')),
-          updatedAt: new Date(Date.parse('2021-12-07 20:06:18.975733')),
+          id: 1411,
+          email: 'example_role1_4@gmail.com',
+          password: '1234',
+          nickname: 'seongyle4',
+          intraId: 4,
+          slack: null,
+          penaltyEndDay: new Date(Date.parse('2022-05-20 07:02:34')),
+          role: 1,
+          createdAt: new Date(Date.parse('2022-05-20 07:02:34.973193')),
+          updatedAt: new Date(Date.parse('2022-05-20 07:02:34.973193')),
         },
         {
-          id: 8,
-          email: '',
-          password: '',
-          nickName: 'yeocho',
-          intraId: 100011,
-          slack: 'U02LEJRST6J',
-          penaltyEndDay: new Date(Date.parse('2021-12-07 20:06:18')),
-          role: 0,
-          createdAt: new Date(Date.parse('2021-12-07 20:06:18.977874')),
-          updatedAt: new Date(Date.parse('2021-12-07 20:06:18.977874')),
-        },
-        {
-          id: 9,
-          email: '',
-          password: '',
-          nickName: 'sumjang',
-          intraId: 100009,
-          slack: 'U02KVE2D2BZ',
-          penaltyEndDay: new Date(Date.parse('2021-12-07 20:06:18')),
-          role: 0,
-          createdAt: new Date(Date.parse('2021-12-07 20:06:18.980172')),
-          updatedAt: new Date(Date.parse('2021-12-07 20:06:18.980172')),
-        },
-        {
-          id: 10,
-          email: '',
-          password: '',
-          nickName: 'jaewchoi',
-          intraId: 100004,
-          slack: 'U02LZQMD63A',
-          penaltyEndDay: new Date(Date.parse('2021-12-07 20:06:18')),
-          role: 0,
-          createdAt: new Date(Date.parse('2021-12-07 20:06:18.983446')),
-          updatedAt: new Date(Date.parse('2021-12-07 20:06:18.983446')),
+          id: 1412,
+          email: 'example_role_1_5@gmail.com',
+          password: '1234',
+          nickname: 'seongyle5',
+          intraId: 5,
+          slack: null,
+          penaltyEndDay: new Date(Date.parse('2022-05-20 07:02:34')),
+          role: 1,
+          createdAt: new Date(Date.parse('2022-05-20 07:02:34.973193')),
+          updatedAt: new Date(Date.parse('2022-05-20 07:02:34.973193')),
         },
       ],
       meta: {
-        totalItems: 1385,
-        itemCount: 5,
-        itemsPerPage: 5,
-        totalPages: 277,
-        currentPage: 2,
+        totalItems: 55,
+        itemCount: 3,
+        itemsPerPage: 3,
+        totalPages: 19,
+        currentPage: 7,
       },
     });
   });
-
 });
