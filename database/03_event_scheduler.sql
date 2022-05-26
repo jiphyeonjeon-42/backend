e9f4cdfdcd9de446b726a92da83bc84d52a6e699
seunam <seunam@student.42seoul.kr>
2022-05-26 15:39:12 +0900

    JIP-81 FIX: add DROP to event_scheduler

--- database/03_event_scheduler.sql
+++ database/03_event_scheduler.sql
@@ -1,6 +1,8 @@
+DROP EVENT IF EXISTS update_reservation_status;
+
 CREATE EVENT update_reservation_status
 ON SCHEDULE EVERY 1 DAY
 STARTS '2022-05-24 00:00:00' ON COMPLETION PRESERVE ENABLE
 DO UPDATE reservation set status = 3 where (status = 0 and endAt <= NOW());
 
-SET GLOBAL event_scheduler = ON
+SET GLOBAL event_scheduler = ON;
