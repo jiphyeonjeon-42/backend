38cc76b78e98a9b66e45e1523dfb45cbd4735c86
yeonseong <yeonseong.dev@gmail.com>
2022-05-26 16:53:02 +0900

    Revert "JIP-59 - JIP-59-books-create-api"

--- backend/src/books/books.type.ts
+++ backend/src/books/books.type.ts
@@ -10,10 +10,3 @@ export interface SortInfoType {
     sort: string;
     limit: string;
 }
-
-export interface CreateBookInfo {
-    isbn: string;
-    donator: string;
-    categoryId: number
-    callSign: string;
-}
