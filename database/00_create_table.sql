d1e0cdd5202c9c02763e1cf8d04ad0c13a693358
YeonSeong-Lee <dltjddus3@gmail.com>
2022-05-30 16:47:26 +0900

    FIX: solve sql syntax

--- database/00_create_table.sql
+++ database/00_create_table.sql
@@ -36,7 +36,7 @@ CREATE TABLE `book` (
   CONSTRAINT `FK_493671e9872dfd0ec4b35c628a2` FOREIGN KEY (`infoId`) REFERENCES `book_info` (`id`),
   CONSTRAINT `FK_donator_id_from_user`
     FOREIGN KEY (`donatorId`)
-      REFERENCES `user`(`id`);
+    REFERENCES `user`(`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=859 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
