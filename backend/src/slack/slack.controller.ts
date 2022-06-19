a362c0350c5900326e653731303cb3dc2abbfe34
Jonghyun Lim <ezemango@gmail.com>
2022-06-19 22:17:18 +0900

    JIP-150 FIX: slack.controller가 에러를 적절히 던집니다.

--- backend/src/slack/slack.controller.ts
+++ backend/src/slack/slack.controller.ts
@@ -6,8 +6,15 @@ export const updateSlackList = async (req: Request, res: Response) : Promise<voi
   try {
     await slack.updateSlackId();
     res.status(204).send();
-  } catch (e: any) {
-    if (e instanceof FtError) res.status(e.statusCode).json({ code: e.errCode, message: e.message });
-    else res.status(500).json({ code: errCode.unknownError, message: errMsg.unknownError });
+  } catch (error: any) {
+    const errorNumber = parseInt(error.message, 10);
+    if (errorNumber >= 400 && errorNumber < 500) {
+      next(new ErrorResponse(error.message, status.BAD_REQUEST));
+    } else if (error.message === 'DB error') {
+      next(new ErrorResponse(errorCode.queryExecutionFailed, status.INTERNAL_SERVER_ERROR));
+    } else {
+      logger.error(error.message);
+      next(new ErrorResponse(errorCode.unknownError, status.INTERNAL_SERVER_ERROR));
+    }
   }
 };
