952e449a9b0a55106022ad3c74845a2e9a5f0db1
YeonSeong-Lee <dltjddus3@gmail.com>
2022-06-23 13:42:17 +0900

    JIP-207 CHORE: delete logger

--- backend/src/reservations/reservations.service.ts
+++ backend/src/reservations/reservations.service.ts
@@ -103,7 +103,6 @@ export const create = async (userId: number, bookInfoId: number) => {
 
 export const
   search = async (query:string, page: number, limit: number, filter: string) => {
-    logger.debug(`reservation search query: ${query} page: ${page} limit ${limit} filter ${filter}`);
     let filterQuery;
     switch (filter) {
       case 'waiting':
@@ -125,7 +124,8 @@ export const
         user.nickname AS login,
         CASE
           WHEN NOW() > user.penaltyEndDate THEN 0
-          ELSE DATEDIFF(now(), user.penaltyEndDate)
+          ELSE DATED
+          IFF(now(), user.penaltyEndDate)
         END AS penaltyDays,
         book.title,
         book.image,
@@ -255,13 +255,11 @@ export const count = async (bookInfoId: number) => {
   if (numberOfBookInfo[0].count > borrowedBookInfo[0].count) {
     throw new Error(errorCode.AVAILABLE_LOAN);
   }
-  logger.debug(`count bookInfoId: ${bookInfoId}`);
   const numberOfReservations = await executeQuery(`
     SELECT COUNT(*) as count
     FROM reservation
     WHERE bookInfoId = ? AND status = 0;
   `, [bookInfoId]);
-  logger.debug(`numberOfReservations: ${numberOfReservations[0].count}`);
   return numberOfReservations[0];
 };
 
@@ -279,7 +277,6 @@ export const reservationKeySubstitution = (obj: queriedReservationInfo): reserva
 };
 
 export const userReservations = async (userId: number) => {
-  logger.debug(`userReservations userId: ${userId}`);
   const reservationList = await executeQuery(`
     SELECT reservation.id as reservationId,
     reservation.bookInfoId as reservedBookInfoId,
@@ -298,6 +295,5 @@ export const userReservations = async (userId: number) => {
     WHERE reservation.userId = ? AND reservation.status = 0;
   `, [userId]) as [queriedReservationInfo];
   reservationList.forEach((obj) => reservationKeySubstitution(obj));
-  logger.debug(`reservationList: ${reservationList}`);
   return reservationList;
 };
