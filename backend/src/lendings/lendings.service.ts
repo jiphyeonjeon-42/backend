b4fe0d2528dddd66e90d5572031fe505acf742ee
Jonghyun Lim <ezemango@gmail.com>
2022-07-28 15:47:57 +0900

    HOTFIX: 예약기능에 버그 안내 메시지 임시 언급

--- backend/src/lendings/lendings.service.ts
+++ backend/src/lendings/lendings.service.ts
@@ -170,7 +170,7 @@ export const returnBook = async (
       // 예약자에게 슬랙메시지 보내기
       const slackIdReservedUser = (await transactionExecuteQuery('SELECT slack from user where id = ?', [isReserved[0].userId]))[0].slack;
       const bookTitle = (await transactionExecuteQuery('SELECT title from book_info where id = (SELECT infoId FROM book WHERE id = ?)', [lendingInfo[0].bookId]))[0].title;
-      publishMessage(slackIdReservedUser, `:jiphyeonjeon: 예약 알림 :jiphyeonjeon:\n예약하신 도서 \`${bookTitle}\`(이)가 대출 가능합니다. 3일 내로 집현전에 방문해 대출해주세요.`);
+      publishMessage(slackIdReservedUser, `:jiphyeonjeon: 예약 알림 :jiphyeonjeon:\n예약하신 도서 \`${bookTitle}\`(이)가 대출 가능합니다. 3일 내로 집현전에 방문해 대출해주세요.\n(현재 예약 기능에 버그가 있습니다. 집현전 웹사이트에서 해당 책이 비치되어있는지 확인해주세요.)`);
     }
     await conn.commit();
     if (isReserved && isReserved[0]) {
