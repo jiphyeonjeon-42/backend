e11f92212d3bc6c4ac679d18d4b1b273ce59a5a3
nyj001012 <nyj001012@gmail.com>
2022-09-15 10:37:21 +0900

    REFACT: 불필요햔 import 삭제

--- backend/src/review/review.service.ts
+++ backend/src/review/review.service.ts
@@ -1,7 +1,6 @@
 import * as errorCode from '../utils/error/errorCode';
 import { executeQuery, makeExecuteQuery, pool } from '../mysql';
 import { Meta } from '../users/users.type';
-import { publishMessage } from '../slack/slack.service';
 
 export const getReview = async () => {
   console.log('get review');
