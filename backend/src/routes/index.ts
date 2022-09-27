e4c2d4a8dd64a27d28fda77e8a90ca5a2527bd9c
nyj001012 <nyj001012@gmail.com>
2022-09-27 16:01:38 +0900

    FEAT: 대출/반납 조회 swagger api 작성

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -5,6 +5,7 @@ import * as lendings from './lendings.routes';
 import * as reservations from './reservations.routes';
 import * as users from './users.routes';
 import * as review from './review.routes';
+import * as history from './history.routes';
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.use(lendings.path, lendings.router);
 router.use(reservations.path, reservations.router);
 router.use(users.path, users.router);
 router.use(review.path, review.router);
+router.use(history.path, history.router);
 
 export default router;
