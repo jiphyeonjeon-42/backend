429be3d24e69f76ad36377a4945005bd62c62a6c
chanhihi <chanhihi55@gmail.com>
2022-10-14 02:48:14 +0900

    feat: final fixed updateBookinfo interface

--- backend/src/books/books.type.ts
+++ backend/src/books/books.type.ts
@@ -26,8 +26,9 @@ export interface UpdateBookInfo {
   title?: string;
   author?: string;
   publisher?: string;
-  isbn?: string;
+  publishedAt?: string | Date;
   image?: string;
   categoryId?: string;
-  publishedAt?: string | Date;
+  callSign: string;
+  status: number;
 }
