e9c9c344d314b59688e04a14325395d325932630
Overraddit <kenwooshim@gmail.com>
2022-10-14 12:56:10 +0900

    FEAT: likes테이블 생성문 추가

--- database/00_create_table.sql
+++ database/00_create_table.sql
@@ -174,6 +174,22 @@ CREATE TABLE `reviews` (
   CONSTRAINT `FK_bookInfo2` FOREIGN KEY (`bookInfoId`) REFERENCES `book_info` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Table structure for table `likes`
+--
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE `likes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` int NOT NULL,
+  `bookInfoId` int  NOT NULL,
+  `isDeleted` boolean NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (`id`),
+  KEY `FK_529dceb01ef681127fef04d755d4` (`userId`),
+  KEY `FK_bookInfo3` (`bookInfoId`),
+  CONSTRAINT `FK_529dceb01ef681127fef04d755d4` FOREIGN KEY (`userId`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_bookInfo3` FOREIGN KEY (`bookInfoId`) REFERENCES `book_info` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
