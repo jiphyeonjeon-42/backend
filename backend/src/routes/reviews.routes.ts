80904c10891cc95773096b3af0d0142f6028c622
Overraddit <kenwooshim@gmail.com>
2022-12-05 18:15:08 +0900

    Feat: add my-reviews api && DOC: specify parameter type

--- backend/src/routes/reviews.routes.ts
+++ backend/src/routes/reviews.routes.ts
@@ -79,29 +79,47 @@ router
      *      tags:
      *      - reviews
      *      parameters:
+     *      - name: limit
+     *        in: query
+     *        schema:
+     *          type: number
+     *        description: 한 페이지에서 몇 개의 게시글을 가져올 지 결정한다. [default = 10]
+     *        required: false
      *      - name: bookInfoId
      *        in: query
+     *        schema:
+     *          type: number
      *        description: book_info 테이블의 id값이 들어오면, 해당 책에 대한 리뷰를 보여준다.
      *        required: false
      *      - name: userId
      *        in: query
+     *        schema:
+     *          type: number
      *        description: user 테이블의 id값이 들어오면, 해당 user가 작성한 리뷰를 보여준다.
      *        required: false
      *      - name: title
      *        in: query
+     *        schema:
+     *          type: string
      *        description: book-info 테이블의 title값이 들어오면, 해당 title의 리뷰를 보여준다.
      *        required: false
      *      - name: intraId
      *        in: query
+     *        schema:
+     *          type: string
      *        description: user 테이블의 intraId값이 들어오면, 해당 user가 작성한 리뷰를 보여준다.
      *        required: false
      *      - name: page
      *        in: query
+     *        schema:
+     *          type: number
      *        description: 해당하는 페이지를 보여준다.
      *        required: false
      *      - name: sort
      *        in: query
-     *        description: asd, desc 값을 통해 시간순으로 정렬된 페이지를 반환한다.
+     *        schema:
+     *          type: string
+     *        description: asc, desc 값을 통해 시간순으로 정렬된 페이지를 반환한다.
      *        required: false
      *      responses:
      *        '200':
@@ -264,6 +282,186 @@ router
      *                     errorCode: 109
      */
     .get('/', authValidate(roleSet.librarian), wrapAsyncController(getReviews));
+
+    router
+    /**
+     * @openapi
+     * /api/reviews/my-reviews:
+     *    get:
+     *      description: 자기자신에 대한 모든 Review 데이터를 가져온다. 내부적으로 getReview와 같은 함수를 사용한다.
+     *      tags:
+     *      - reviews
+     *      parameters:
+     *      - name: limit
+     *        in: query
+     *        schema:
+     *          type: number
+     *        description: 한 페이지에서 몇 개의 게시글을 가져올 지 결정한다. [default = 10]
+     *        required: false
+     *      - name: page
+     *        in: query
+     *        schema:
+     *          type: number
+     *        description: 해당하는 페이지를 보여준다.
+     *        required: false
+     *      - name: sort
+     *        in: query
+     *        schema:
+     *          type: string
+     *        description: asd, desc 값을 통해 시간순으로 정렬된 페이지를 반환한다.
+     *        required: false
+     *      responses:
+     *        '200':
+     *           content:
+     *             application/json:
+     *               schema:
+     *                 type: object
+     *               examples:
+     *                bookInfo 기준 :
+     *                  value:
+     *                    items : [
+     *                      {
+     *                      reviewsId : 1,
+     *                      reviewerId : 100,
+     *                      bookInfoId: 1,
+     *                      content : "hello",
+     *                      createdAt: "2022-11-09T06:56:15.640Z",
+     *                      title: "클린코드",
+     *                      nickname : "sechung",
+     *                      intraId: "default@student.42seoul.kr",
+     *                      },
+     *                      {
+     *                      reviewsId : 2,
+     *                      reviewerId : 101,
+     *                      bookInfoId: 1,
+     *                      content : "hello",
+     *                      createdAt: "2022-11-09T06:56:15.640Z",
+     *                      title: "클린코드",
+     *                      nickname : "chanheki",
+     *                      intraId: "default@student.42seoul.kr",
+     *                      },
+     *                      {
+     *                      reviewsId : 3,
+     *                      reviewerId : 102,
+     *                      bookInfoId: 1,
+     *                      content : "hello",
+     *                      createdAt: "2022-11-09T06:56:15.640Z",
+     *                      title: "클린코드",
+     *                      nickname : "chanheki",
+     *                      intraId: "default@student.42seoul.kr",
+     *                      },
+     *                      {
+     *                      reviewsId : 4,
+     *                      reviewerId : 103,
+     *                      bookInfoId: 1,
+     *                      content : "hello",
+     *                      createdAt: "2022-11-09T06:56:15.640Z",
+     *                      title: "클린코드",
+     *                      nickname : "chanheki",
+     *                      intraId: "default@student.42seoul.kr",
+     *                      },
+     *                      {
+     *                      reviewsId : 5,
+     *                      reviewerId : 104,
+     *                      bookInfoId: 1,
+     *                      content : "hello",
+     *                      createdAt: "2022-11-09T06:56:15.640Z",
+     *                      title: "클린코드",
+     *                      nickname : "chanheki",
+     *                      intraId: "default@student.42seoul.kr",
+     *                      },
+     *                      {
+     *                      reviewsId : 6,
+     *                      reviewerId : 105,
+     *                      bookInfoId: 1,
+     *                      content : "hello",
+     *                      createdAt: "2022-11-09T06:56:15.640Z",
+     *                      title: "클린코드",
+     *                      nickname : "chanheki",
+     *                      intraId: "default@student.42seoul.kr",
+     *                      },
+     *                      {
+     *                      reviewsId : 7,
+     *                      reviewerId : 106,
+     *                      bookInfoId: 1,
+     *                      content : "hello",
+     *                      createdAt: "2022-11-09T06:56:15.640Z",
+     *                      title: "클린코드",
+     *                      nickname : "chanheki",
+     *                      intraId: "default@student.42seoul.kr",
+     *                      },
+     *                      {
+     *                      reviewsId : 8,
+     *                      reviewerId : 107,
+     *                      bookInfoId: 1,
+     *                      content : "hello",
+     *                      createdAt: "2022-11-09T06:56:15.640Z",
+     *                      title: "클린코드",
+     *                      nickname : "chanheki",
+     *                      intraId: "default@student.42seoul.kr",
+     *                      },
+     *                      {
+     *                      reviewsId : 9,
+     *                      reviewerId : 108,
+     *                      bookInfoId: 1,
+     *                      content : "hello",
+     *                      createdAt: "2022-11-09T06:56:15.640Z",
+     *                      title: "클린코드",
+     *                      nickname : "chanheki",
+     *                      intraId: "default@student.42seoul.kr",
+     *                      },
+     *                      {
+     *                      reviewsId : 10,
+     *                      reviewerId : 109,
+     *                      bookInfoId: 1,
+     *                      content : "hello",
+     *                      createdAt: "2022-11-09T06:56:15.640Z",
+     *                      title: "클린코드",
+     *                      nickname : "chanheki",
+     *                      intraId: "default@student.42seoul.kr",
+     *                      },
+     *                      ]
+     *                    "meta": {
+     *                        totalItems: 100,
+     *                        itemCount : 10,
+     *                        itemsPerPage : 10,
+     *                        totalPages : 20,
+     *                        currentPage : 1,
+     *                        finalPage : False
+     *                      }
+     *        '400':
+     *           content:
+     *             application/json:
+     *               schema:
+     *                 type: object
+     *               examples:
+     *                적절하지 않는 page 값:
+     *                  value:
+     *                    errorCode: 2
+     *                적절하지 않는 sort 값:
+     *                  value:
+     *                    errorCode: 2
+     *        '401':
+     *           description: 권한 없음.
+     *           content:
+     *             application/json:
+     *               schema:
+     *                 type: object
+     *               examples:
+     *                 토큰 누락 :
+     *                   value:
+     *                     errorCode: 100
+     *                 토큰 유저 존재하지 않음 :
+     *                   value :
+     *                     errorCode: 101
+     *                 토큰 만료 :
+     *                   value :
+     *                     errorCode: 108
+     *                 토큰 유효하지 않음 :
+     *                   value :
+     *                     errorCode: 109
+     */
+    .get('/my-reviews', authValidate(roleSet.all), wrapAsyncController(getReviews));
 
 router
     /**
