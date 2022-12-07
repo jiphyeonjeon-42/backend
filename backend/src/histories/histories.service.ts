bd7ec399dbcf4b75fb4c44c897b6831ec3f7bfdb
yeonseong <yeonseong.dev@gmail.com>
2022-12-07 21:57:15 +0900

    FIX: 정렬기준 추가

--- backend/src/histories/histories.service.ts
+++ backend/src/histories/histories.service.ts
@@ -59,7 +59,7 @@ export const histories = async (
     JOIN book ON book.id = lending.bookId
     JOIN book_info ON book_info.id = book.infoID
     ${filterQuery}
-    ORDER BY createdAt DESC
+    ORDER BY createdAt DESC, user.nickname DESC
     LIMIT ?
     OFFSET ?
   `,
