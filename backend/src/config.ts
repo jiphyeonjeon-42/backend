d560083887dca9c9f4fc946fb753855173e994bd
jhmin95 <tesla52@naver.com>
2022-12-22 15:52:47 +0900

    RDS 관련 환경변수로 변경

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -6,11 +6,11 @@ const config = {
   nodeEnv: process.env.NODE_ENV,
   mode: process.env.MODE,
   database: {
-    host: process.env.MODE === 'local' ? 'localhost' : 'database',
+    host: process.env.MODE === 'local' ? process.env.RDS_HOSTNAME : 'database',
     port: 3306,
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    dbName: process.env.MYSQL_DATABASE,
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    dbName: process.env.RDS_DB_NAME,
   },
   client: {
     id: process.env.CLIENT_ID,
