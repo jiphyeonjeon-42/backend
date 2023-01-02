6af5afabcd5a386bc3aa4d8c984c30d1d2864a01
nyj001012 <nyj001012@gmail.com>
2023-01-02 17:37:11 +0900

    FEAT: userReservations() 전용 View Entity 추가

---
+++ backend/src/entity/entities/UserReservation.ts
@@ -0,0 +1,57 @@
+import { ViewEntity, ViewColumn, DataSource } from 'typeorm';
+import BookInfo from './BookInfo';
+import Reservation from './Reservation';
+
+@ViewEntity({
+  expression: (Data: DataSource) => Data
+    .createQueryBuilder()
+    .select('r.id', 'reservationId')
+    .addSelect('r.bookInfoId', 'reservedBookInfoId')
+    .addSelect('r.createdAt', 'reservationDate')
+    .addSelect('r.endAt', 'endAt')
+    .addSelect(
+      `(SELECT COUNT(*)
+       FROM reservation
+       WHERE (status = 0) 
+        AND (bookInfoId = reservedBookInfoId) 
+        AND (createdAt <= reservationDate))`,
+      'ranking',
+    )
+    .addSelect('bi.title', 'title')
+    .addSelect('bi.author', 'author')
+    .addSelect('bi.image', 'image')
+    .addSelect('r.userId', 'userId')
+    .from(Reservation, 'r')
+    .leftJoin(BookInfo, 'bi', 'r.bookInfoId = bi.id')
+    .where('r.status = 0'),
+})
+class UserReservation {
+  @ViewColumn()
+  reservationId: number;
+
+  @ViewColumn()
+  reservedBookInfoId: number;
+
+  @ViewColumn()
+  reservationDate: Date;
+
+  @ViewColumn()
+  endAt: Date;
+
+  @ViewColumn()
+  ranking: number;
+
+  @ViewColumn()
+  title: string;
+
+  @ViewColumn()
+  author: string;
+
+  @ViewColumn()
+  image: string;
+
+  @ViewColumn()
+  userId: number;
+}
+
+export default UserReservation;
