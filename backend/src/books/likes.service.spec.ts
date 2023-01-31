3d689d7791376d6f863eec0ca206f4d5466a70b3
yeonseong <yeonseong.dev@gmail.com>
2023-01-31 17:19:03 +0900

    FEAT: init test
    Co-authored-by: 숯개발자 <OverRaddit@users.noreply.github.com>

---
+++ backend/src/books/likes.service.spec.ts
@@ -0,0 +1,10 @@
+import * as BooksService from "./books.service";
+
+describe("Like Test", () => {
+  afterAll(async () => {});
+
+  //TODO: createLike의 케이스들 나누어두기
+  test("test", async () => {
+    //expect(await BooksService.createLike(1, 1)).toEqual({ errorCode: 401 });
+  });
+});
