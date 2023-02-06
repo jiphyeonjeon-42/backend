69035ba98aaeeb1bb23a1fb2c49bf1ecded5826b
yeonseong <yeonseong.dev@gmail.com>
2023-02-07 01:53:46 +0900

    FEAT: Likes 레포 생성

--- backend/src/books/Likes.repository.ts
+++ backend/src/books/Likes.repository.ts
@@ -1,13 +1,26 @@
-import { Repository } from "typeorm";
-import jipDataSource from "../app-data-source";
-import Likes from "../entity/entities/Likes";
+import { QueryRunner, Repository } from 'typeorm';
+import jipDataSource from '../app-data-source';
+import Likes from '../entity/entities/Likes';
 
 class LikesRepository extends Repository<Likes> {
+  private transactionQueryRunner: QueryRunner | null;
+
   constructor() {
     super(
       Likes,
       jipDataSource.createEntityManager(),
-      jipDataSource.createQueryRunner()
+      jipDataSource.createQueryRunner(),
     );
   }
+
+  async getLikesByBookInfoId(bookInfoId: number) : Promise<Likes[]> {
+    const likes = await this.find({
+      where: {
+        bookInfoId,
+      },
+    });
+    return likes;
+  }
 }
+
+export = new LikesRepository();
