22a3c361dab504129616406d819d1465554d8024
jhmin95 <tesla52@naver.com>
2023-02-20 21:06:02 +0900

    FIX: authValidate 지워두기 (서비스에서 살릴 예정)

--- backend/src/routes/stock.routes.ts
+++ backend/src/routes/stock.routes.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 import { stockSearch, stockUpdate } from '../stocks/stocks.controller';
-import authValidate from '../auth/auth.validate';
-import { roleSet } from '../auth/auth.type';
 
 export const path = '/stock';
 
 export const router = Router();
 
-router.get('/search', authValidate(roleSet.librarian), stockSearch)
-  .patch('/update', authValidate(roleSet.librarian), stockUpdate);
+router.get('/search', stockSearch)
+  .patch('/update', stockUpdate);
