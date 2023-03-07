269ef6df2745aff12a7dd92de6cbbec6f34ffd4d
Overraddit <kenwooshim@gmail.com>
2023-03-08 01:09:48 +0900

    Fix: fix v_lending_for_search_user.reservedNum

--- backend/src/entity/entities/VLendingForSearchUser.ts
+++ backend/src/entity/entities/VLendingForSearchUser.ts
@@ -12,7 +12,7 @@ import { DataSource, ViewColumn, ViewEntity } from 'typeorm';
     .addSelect('bi.title', 'title')
     .addSelect('DATE_ADD(l.createdAt, INTERVAL 14 DAY)', 'duedate')
     .addSelect('CASE WHEN DATEDIFF(now(), DATE_ADD(l.createdAt, INTERVAL 14 DAY)) < 0 THEN 0 ELSE DATEDIFF(now(), DATE_ADD(l.createdAt, INTERVAL 14 DAY)) END', 'overDueDay')
-    .addSelect('(SELECT COUNT(r.id) FROM reservation r WHERE r.userId = l.userId)', 'reservedNum')
+    .addSelect('(SELECT COUNT(r.id) FROM reservation r WHERE r.bookInfoId = bi.id AND r.status = 0)', 'reservedNum')
     .from('lending', 'l')
     .where('l.returnedAt is NULL')
     .innerJoin('user', 'u', 'l.userId = u.id')
