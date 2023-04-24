47f53c39007cbaf72996e4ec73936b723415ab09
cjho0316 <cjho03160316@gmail.com>
2023-04-24 17:24:27 +0900

    feat(tag): 컨트롤러 추가
    컨트롤러 추가

---
+++ backend/src/tags/tags.controller.ts
@@ -0,0 +1,23 @@
+import {
+    NextFunction, Request, Response,
+  } from 'express';
+  import * as status from 'http-status';
+  import * as tagsService from './tags.service';
+  import ErrorResponse from '../utils/error/errorResponse';
+  import { logger } from '../utils/logger';
+  import * as errorCode from '../utils/error/errorCode';
+  
+  const tagsService = new tagsService();
+
+  export const createDefaultTags = async (
+    req: Request,
+    res: Response,
+  ) => {
+    const { id: tokenId } = req.user as any;
+    const bookInfoId = req?.body?.bookInfoId;
+    const content = req?.body?.content;
+    contentParseCheck(content);
+    await tagsService.createTags(tokenId, bookInfoId, content);
+    return res.status(status.OK).send();
+  };
+  
