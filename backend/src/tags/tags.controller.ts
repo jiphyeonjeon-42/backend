29149f6cc946a368e6722a074949451fc2380ff3
yena <50291995+nyj001012@users.noreply.github.com>
2023-04-25 09:02:14 +0900

    chore(tag): eslint 수정
    eslint 수정

--- backend/src/tags/tags.controller.ts
+++ backend/src/tags/tags.controller.ts
@@ -1,23 +1,20 @@
 import {
-    NextFunction, Request, Response,
-  } from 'express';
-  import * as status from 'http-status';
-  import tagsService from './tags.service';
-  import ErrorResponse from '../utils/error/errorResponse';
-  import { logger } from '../utils/logger';
-  import * as errorCode from '../utils/error/errorCode';
-    
-  
+  NextFunction, Request, Response,
+} from 'express';
+import * as status from 'http-status';
+import tagsService from './tags.service';
+import ErrorResponse from '../utils/error/errorResponse';
+import { logger } from '../utils/logger';
+import * as errorCode from '../utils/error/errorCode';
 
-  export const createDefaultTags = async (
-    req: Request,
-    res: Response,
-  ) => {
-    const { id: tokenId } = req.user as any;
-    const bookInfoId = req?.body?.bookInfoId;
-    const content = req?.body?.content;
-    // contentParseCheck(content);
-    await tagsService.createDefaultTags(tokenId, bookInfoId, content);
-    return res.status(status.OK).send();
-  };
-  
+export const createDefaultTags = async (
+  req: Request,
+  res: Response,
+) => {
+  const { id: tokenId } = req.user as any;
+  const bookInfoId = req?.body?.bookInfoId;
+  const content = req?.body?.content;
+  // contentParseCheck(content);
+  await tagsService.createDefaultTags(tokenId, bookInfoId, content);
+  return res.status(status.OK).send();
+};
