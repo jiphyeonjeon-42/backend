cff87e351e95c2d6f1dfc15d21ec4ee83a39616c
yena <50291995+nyj001012@users.noreply.github.com>
2023-04-25 09:00:07 +0900

    chore(tag): eslint 수정
    eslint 수정

--- backend/src/tags/tags.repository.ts
+++ backend/src/tags/tags.repository.ts
@@ -5,7 +5,7 @@ import SubTag from '../entity/entities/SubTag';
 import * as errorCode from '../utils/error/errorCode';
 import BookInfo from '../entity/entities/BookInfo';
 import User from '../entity/entities/User';
-import ErrorResponse from "../utils/error/errorResponse";
+import ErrorResponse from '../utils/error/errorResponse';
 
 export default class SubTagRepository extends Repository<SubTag> {
   private readonly bookInfoRepo: Repository<BookInfo>;
@@ -22,12 +22,12 @@ export default class SubTagRepository extends Repository<SubTag> {
 
   async createDefaultTags(userId: number, bookInfoId: number, content: string): Promise<void> {
     const insertObject: QueryDeepPartialEntity<SubTag> = {
-        superTagId: 0,
-        userid: userId,
-        bookInfoId: bookInfoId,
-        content: content,
-        updateUserId: userId
-    }
+      superTagId: 0,
+      userid: userId,
+      bookInfoId,
+      content,
+      updateUserId: userId,
+    };
     await this.insert(insertObject);
   }
 }
