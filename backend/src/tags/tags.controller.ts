cfb57a0fbf3d4e97d0ad686212e54c057b288c47
yena <50291995+nyj001012@users.noreply.github.com>
2023-04-26 13:26:52 +0900

    feat(tag): 서브 및 디폴트 태그를 조회하는 컨트롤러 추가
    서브 및 디폴트 태그를 조회하는 컨트롤러 추가

--- backend/src/tags/tags.controller.ts
+++ backend/src/tags/tags.controller.ts
@@ -3,6 +3,7 @@ import {
 } from 'express';
 import * as status from 'http-status';
 import tagsService from './tags.service';
+import * as parseCheck from '../utils/parseCheck';
 import ErrorResponse from '../utils/error/errorResponse';
 import { logger } from '../utils/logger';
 import * as errorCode from '../utils/error/errorCode';
@@ -17,4 +18,16 @@ export const createDefaultTags = async (
   // contentParseCheck(content);
   await tagsService.createDefaultTags(tokenId, bookInfoId, content);
   return res.status(status.OK).send();
+};
+
+export const searchSubDefaultTags = async (
+  req: Request,
+  res: Response,
+) => {
+  const page: number = parseCheck.pageParse(parseInt(String(req?.query?.page), 10));
+  const limit: number = parseCheck.limitParse(parseInt(String(req?.query?.limit), 10));
+  const visibility: string = parseCheck.stringQueryParse(req?.query?.visibility);
+  const title: string = parseCheck.stringQueryParse(req?.query?.title);
+  return res.status(status.OK)
+    .json(await tagsService.searchSubDefaultTags(page, limit, visibility, title));
 };
