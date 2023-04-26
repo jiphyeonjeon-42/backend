dcfc88ada64b628168ab7af3045b7abe3599e05a
yena <50291995+nyj001012@users.noreply.github.com>
2023-04-26 13:28:18 +0900

    feat(tag): 서브 및 디폴트 태그를 조회하는 서비스 추가
    서브 및 디폴트 태그를 조회하는 서비스 추가

--- backend/src/tags/tags.service.ts
+++ backend/src/tags/tags.service.ts
@@ -1,4 +1,5 @@
 // import * as errorCheck from '../utils/errorCheck';
+import { Like } from 'typeorm';
 import TagsRepository from './tags.repository';
 
 export class TagsService {
@@ -11,6 +12,39 @@ export class TagsService {
   async createDefaultTags(userId: number, bookInfoId: number, content: string) {
     await this.tagsRepository.createDefaultTags(userId, bookInfoId, content);
   }
+
+  async searchSubDefaultTags(page: number, limit: number, visibility: string, title: string)
+  : Promise<Object> {
+    const conditions: any = {};
+    switch (visibility) {
+      case 'public':
+        conditions.isPublic = 1;
+        break;
+      case 'private':
+        conditions.isPublic = 0;
+        break;
+      default:
+        conditions.isPublic = 1;
+        break;
+    }
+    if (title) { conditions.title = Like(`%${title}%`); }
+    const [items, count] = await this.tagsRepository.searchSubDefaultTags(
+      page,
+      limit,
+      conditions,
+    );
+    const itemPerPage = (Number.isNaN(limit)) ? 10 : limit;
+    const meta = {
+      totalItems: count,
+      itemPerPage,
+      totalPages: parseInt(String(count / itemPerPage
+        + Number((count % itemPerPage !== 0) || !count)), 10),
+      firstPage: page === 0,
+      finalPage: page === parseInt(String(count / itemPerPage), 10),
+      currentPage: page,
+    };
+    return { items, meta };
+  }
 }
 
-export default new TagsService()
+export default new TagsService();
