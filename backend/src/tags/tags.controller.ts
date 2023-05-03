5228a1a01f1caee00457f8ffb65e707cc48b90e5
yena <50291995+nyj001012@users.noreply.github.com>
2023-05-03 13:37:18 +0900

    chore(tag): checkout 후 merge된 부분 수정
    merge가 잘못된 부분 수정

--- backend/src/tags/tags.controller.ts
+++ backend/src/tags/tags.controller.ts
@@ -3,9 +3,8 @@ import {
 } from 'express';
 import * as status from 'http-status';
 import * as parseCheck from '../utils/parseCheck';
-import TagsService from './tags.service';
+import { TagsService } from './tags.service';
 import ErrorResponse from '../utils/error/errorResponse';
-import { logger } from '../utils/logger';
 import * as errorCode from '../utils/error/errorCode';
 
 export const createDefaultTags = async (
@@ -53,16 +52,16 @@ export const searchSuperDefaultTags = async (
 };
 
 export const createSuperTags = async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ) => {
-    const { id: tokenId } = req.user as any;
-    const bookInfoId = req?.body?.bookInfoId;
-    const content = req?.body?.content;
-    const tagsService = new TagsService();
-    const regex: RegExp = /^[A-Za-zㅎ가-힣0-9_]+$/;
-    if (content === '' || content === 'default' || content.length > 42 || regex.test(content) === false) next(new ErrorResponse(errorCode.INVALID_INPUT_TAGS, 400));
-    await tagsService.createSuperTags(tokenId, bookInfoId, content);
-    return res.status(status.CREATED).send();
-  };
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { id: tokenId } = req.user as any;
+  const bookInfoId = req?.body?.bookInfoId;
+  const content = req?.body?.content;
+  const tagsService = new TagsService();
+  const regex: RegExp = /^[A-Za-zㅎ가-힣0-9_]+$/;
+  if (content === '' || content === 'default' || content.length > 42 || regex.test(content) === false) next(new ErrorResponse(errorCode.INVALID_INPUT_TAGS, 400));
+  await tagsService.createSuperTags(tokenId, bookInfoId, content);
+  return res.status(status.CREATED).send();
+};
