cbdb35f74984e025cfa93945ea93ae860b750ab3
yena <50291995+nyj001012@users.noreply.github.com>
2023-05-23 15:21:43 +0900

    chore(tag): merge 후 동작하지 않는 부분 수정
    merge 후 동작하지 않는 부분 수정

--- backend/src/tags/tags.controller.ts
+++ backend/src/tags/tags.controller.ts
@@ -4,7 +4,6 @@ import {
 import * as status from 'http-status';
 import TagsService from './tags.service';
 import ErrorResponse from '../utils/error/errorResponse';
-import { logger } from '../utils/logger';
 import * as parseCheck from '../utils/parseCheck';
 import * as errorCode from '../utils/error/errorCode';
 
@@ -24,42 +23,42 @@ export const createDefaultTags = async (
 };
 
 export const createSuperTags = async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ) => {
-    const { id: tokenId } = req.user as any;
-    const bookInfoId = req?.body?.bookInfoId;
-    const content = req?.body?.content.trim();
-    const tagsService = new TagsService();
-    const regex: RegExp = /^[A-Za-zㅎ가-힣0-9_]+$/;
-    if (content === '' || content === 'default' || content.length > 42 || regex.test(content) === false) next(new ErrorResponse(errorCode.INVALID_INPUT_TAGS, 400));
-    await tagsService.createSuperTags(tokenId, bookInfoId, content);
-    return res.status(status.CREATED).send();
-  };
-  
-  export const deleteSuperTags = async (
-    req: Request,
-    res: Response,
-  ) => {
-    const { id: tokenId } = req.user as any;
-    const superTagId = req?.params?.tagId;
-    const tagsService = new TagsService();
-    await tagsService.deleteSuperTag(parseInt(superTagId, 10), tokenId);
-    return res.status(status.OK).send();
-  };
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { id: tokenId } = req.user as any;
+  const bookInfoId = req?.body?.bookInfoId;
+  const content = req?.body?.content.trim();
+  const tagsService = new TagsService();
+  const regex: RegExp = /^[A-Za-zㅎ가-힣0-9_]+$/;
+  if (content === '' || content === 'default' || content.length > 42 || regex.test(content) === false) next(new ErrorResponse(errorCode.INVALID_INPUT_TAGS, 400));
+  await tagsService.createSuperTags(tokenId, bookInfoId, content);
+  return res.status(status.CREATED).send();
+};
 
-  export const deleteSubTags = async (
-    req: Request,
-    res: Response,
-  ) => {
-    const { id: tokenId } = req.user as any;
-    const subTagId = req?.params?.tagId;
-    const tagsService = new TagsService();
-    await tagsService.deleteSubTag(parseInt(subTagId, 10), tokenId);
-    return res.status(status.OK).send();
-  };
-=======
+export const deleteSuperTags = async (
+  req: Request,
+  res: Response,
+) => {
+  const { id: tokenId } = req.user as any;
+  const superTagId = req?.params?.tagId;
+  const tagsService = new TagsService();
+  await tagsService.deleteSuperTag(parseInt(superTagId, 10), tokenId);
+  return res.status(status.OK).send();
+};
+
+export const deleteSubTags = async (
+  req: Request,
+  res: Response,
+) => {
+  const { id: tokenId } = req.user as any;
+  const subTagId = req?.params?.tagId;
+  const tagsService = new TagsService();
+  await tagsService.deleteSubTag(parseInt(subTagId, 10), tokenId);
+  return res.status(status.OK).send();
+};
+
 export const searchSubDefaultTags = async (
   req: Request,
   res: Response,
@@ -87,21 +86,6 @@ export const searchSuperDefaultTags = async (
 ) => {
   const bookInfoId: number = parseInt(req.params.bookInfoId, 10);
   return res.status(status.OK).json(await new TagsService().searchSuperDefaultTags(bookInfoId));
-};
-
-export const createSuperTags = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const { id: tokenId } = req.user as any;
-  const bookInfoId = req?.body?.bookInfoId;
-  const content = req?.body?.content;
-  const tagsService = new TagsService();
-  const regex: RegExp = /^[A-Za-zㅎ가-힣0-9_]+$/;
-  if (content === '' || content === 'default' || content.length > 42 || regex.test(content) === false) next(new ErrorResponse(errorCode.INVALID_INPUT_TAGS, 400));
-  await tagsService.createSuperTags(tokenId, bookInfoId, content);
-  return res.status(status.CREATED).send();
 };
 
 export const mergeTags = async (
