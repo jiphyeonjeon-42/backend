bcfdcaf6e6113f67df264529b008b670233355f2
yena <50291995+nyj001012@users.noreply.github.com>
2023-05-24 19:42:50 +0900

    Update backend/src/DTO/tags.model.ts
    Co-authored-by: sujikim <sujikim@student.42seoul.kr>

--- backend/src/DTO/tags.model.ts
+++ backend/src/DTO/tags.model.ts
@@ -6,7 +6,7 @@ export interface subDefaultTag {
   login: string;
   content: string;
   superContent: string;
-  visibility: string;
+  visibility: "public" | "private";
 }
 
 export interface superDefaultTag {
