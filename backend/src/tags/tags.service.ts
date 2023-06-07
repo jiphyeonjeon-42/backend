7c61735f3975b50eb9c094158f4589e62b7127d6
cjho0316 <cjho03160316@gmail.com>
2023-06-07 19:24:01 +0900

    feat(tag): createSuperTags의 return생성
    createSuperTags의 return생성

--- backend/src/tags/tags.service.ts
+++ backend/src/tags/tags.service.ts
@@ -114,9 +114,24 @@ export class TagsService {
   }
 
   async createSuperTags(userId: number, bookInfoId: number, content: string) {
+    
+    let superTagsInsertion: superDefaultTag;
     try {
-      await this.queryRunner.startTransaction();
-      await this.superTagRepository.createSuperTag(content, bookInfoId, userId);
+      await this.queryRunner.startTransaction(); 
+      const superTagId = await this.superTagRepository.createSuperTag(content, bookInfoId, userId);
+      const superTag = await this.superTagRepository.getSuperTags({ id: superTagId });
+      const superLogin: string | null = await this.superTagRepository.getSuperTagLogin(superTagId);
+      
+      if (superLogin === null)
+        throw new Error(errorCode.CREATE_FAIL_TAGS);
+
+      superTagsInsertion = {
+        id: superTag[0].id,
+        content: superTag[0].content,
+        login: superLogin,
+        count: 0,
+        type: 'super',
+      };
       await this.queryRunner.commitTransaction();
     } catch (e) {
       await this.queryRunner.rollbackTransaction();
@@ -124,8 +139,9 @@ export class TagsService {
     } finally {
       await this.queryRunner.release();
     }
+    return superTagsInsertion;
   }
-
+  
   async deleteSuperTag(superTagsId: number, deleteUser: number) {
     await this.superTagRepository.deleteSuperTag(superTagsId, deleteUser);
   }
