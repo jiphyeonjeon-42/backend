22b3a7f4e93d79a818f268624ea08cd6646e9901
scarf <greenscarf005@gmail.com>
2023-06-20 09:15:52 +0900

    feat: .env로부터 DB 옵션 파싱을 타입 안전하게 변경

--- backend/src/app-data-source.ts
+++ backend/src/app-data-source.ts
@@ -1,51 +1,17 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
-import dotenv from 'dotenv';
-import { logger } from './utils/logger';
+import { connectOption } from './env';
 
-dotenv.config();
-
-let hostName;
-let username;
-let password;
-let database;
-
-switch (process.env.MODE) {
-  case 'local':
-    hostName = 'localhost';
-    username = process.env.MYSQL_USER;
-    password = process.env.MYSQL_PASSWORD;
-    database = process.env.MYSQL_DATABASE;
-    break;
-  case 'RDS':
-    hostName = process.env.RDS_HOSTNAME;
-    username = process.env.RDS_USERNAME;
-    password = process.env.RDS_PASSWORD;
-    database = process.env.RDS_DB_NAME;
-    break;
-  case 'prod':
-    hostName = 'database';
-    username = process.env.MYSQL_USER;
-    password = process.env.MYSQL_PASSWORD;
-    database = process.env.MYSQL_DATABASE;
-    break;
-  default:
-    logger.error("no valid env mode");
-}
-
-export const option = {
+export const option: DataSourceOptions = {
   type: 'mysql',
-  host: hostName,
   port: 3306,
-  username,
-  password,
-  database,
+  ...connectOption,
   entities: [
     `${__dirname}/**/entities/*.{js,ts}`,
   ],
   logging: true,
-//  synchronize: true,
+  //  synchronize: true,
   poolSize: 200,
-} as DataSourceOptions;
+};
 console.log(__dirname);
 const jipDataSource = new DataSource(option);
 
