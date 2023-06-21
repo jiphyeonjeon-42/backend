0aa9378378052e7a58716d12516c9ec635cadea1
scarf <greenscarf005@gmail.com>
2023-06-21 18:47:59 +0900

    refactor: config.ts에서 connectOption 사용

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,44 +1,14 @@
 import dotenv from 'dotenv';
+import { connectOption } from './env';
 
 dotenv.config();
-
-let hostName;
-let username;
-let password;
-let database;
-
-switch (process.env.MODE) {
-  case 'local':
-    hostName = 'local';
-    username = process.env.MYSQL_USER;
-    password = process.env.MYSQL_PASSWORD;
-    database = process.env.MYSQL_DATABASE;
-    break;
-  case 'RDS':
-    hostName = process.env.RDS_HOSTNAME;
-    username = process.env.RDS_USERNAME;
-    password = process.env.RDS_PASSWORD;
-    database = process.env.RDS_DB_NAME;
-    break;
-  case 'prod':
-    hostName = 'database';
-    username = process.env.MYSQL_USER;
-    password = process.env.MYSQL_PASSWORD;
-    database = process.env.MYSQL_DATABASE;
-    break;
-  default:
-    hostName = 'database';
-}
 
 const config = {
   nodeEnv: process.env.NODE_ENV,
   mode: process.env.MODE,
   database: {
-    host: hostName,
     port: 3306,
-    username,
-    password,
-    dbName: database,
+    ...connectOption,
   },
   client: {
     id: process.env.CLIENT_ID,
