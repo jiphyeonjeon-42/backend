10dced563d350d00f88ff879bf1d7f32a9e7995f
scarf <greenscarf005@gmail.com>
2023-06-24 19:52:50 +0900

    chore: 사용하지 않는 config의 nodeEnv와 mode 필드 제거

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,8 +4,6 @@ import { connectOption, oauth42ApiOption, oauthUrlOption } from './env';
 dotenv.config();
 
 const config = {
-  nodeEnv: process.env.NODE_ENV,
-  mode: process.env.MODE,
   database: {
     port: 3306,
     ...connectOption,
