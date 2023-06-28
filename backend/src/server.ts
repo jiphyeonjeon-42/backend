c3f6910045419ab2ae621cd57d051af9f4e38719
scarf <greenscarf005@gmail.com>
2023-06-28 10:20:47 +0900

    fix: graceful shutdown 구현
    nodemon등으로 빠르게 재시작시 기존 포트가 사용중이어서 오류가 발생하는 경우가 있습니다.
    종료 및 재시작 시그널을 받을 시 기존 연결을 끊고 종료하도록 수정하였습니다.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,13 @@
+import { createHttpTerminator } from 'http-terminator';
 import app from './app';
+import jipDataSource from './app-data-source';
+import { logger } from './utils/logger';
 import scheduler from './utils/scheduler';
+import { gracefulTerminationTimeout } from './config';
 
 const port = '3000';
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`
   ################################################
   🛡️  Server listening on port: ${port}🛡️
@@ -11,3 +15,24 @@ app.listen(port, () => {
     `);
   scheduler();
 });
+
+const httpTerminator = createHttpTerminator({ server, gracefulTerminationTimeout });
+
+/** 종료 시그널을 받으면 할당된 자원(typeorm DB 연결, 소켓)을 반환하고 서버를 종료합니다. */
+const attemptGracefulShutdown = async (signal: string) => {
+  logger.warn(`Attempting to gracefully shutdown for ${signal}`);
+  if (jipDataSource.isInitialized) {
+    try {
+      await jipDataSource.destroy();
+      logger.warn('successfuly closed typeorm connection to database');
+    } catch (error) {
+      logger.error('error when closing database connection', error);
+    }
+  }
+  await httpTerminator.terminate();
+  logger.warn('closed http server');
+  process.exit(0);
+};
+
+const signals = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
+signals.forEach((signal) => process.on(signal, () => attemptGracefulShutdown(signal)));
