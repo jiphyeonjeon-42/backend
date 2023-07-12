533a76508a180c0c0185ebbb4c6e17b4749496cc
jhmin95 <tesla52@naver.com>
2023-07-12 19:18:18 +0900

    chore: add annotations

--- contracts/src/index.ts
+++ contracts/src/index.ts
@@ -4,10 +4,14 @@ import { initContract } from '@ts-rest/core';
 import { z } from 'zod';
 import { reviewsIdSchema } from './reviews';
 
+//  zod 스키마를 확장하여 openapi 스키마를 생성
+//  openapi == swagger
 extendZodWithOpenApi(z);
 
+//  initContract 함수를 통해 contract 를 생성
 const c = initContract();
 
+//  contract 를 생성할 때, router 함수를 사용하여 api 를 생성
 export const reviewsContract = c.router(
   {
     patch: {
@@ -15,6 +19,7 @@ export const reviewsContract = c.router(
       path: '/:reviewsId',
       description: '책 리뷰의 비활성화 여부를 토글 방식으로 변환합니다.',
       pathParams: z.object({
+        //  zod 스키마
         reviewsId: reviewsIdSchema.openapi({ example: 1 }),
       }),
       body: null,
@@ -30,9 +35,12 @@ export const reviewsContract = c.router(
       },
     },
   },
+  //  pathPrefix 를 통해 모든 경로에 공통적으로 /reviews 를 붙여줍니다.
+  //  여러 api 마다 공통적으로 /reviews/~~~ 를 사용하니까 중복해서 선언하는 것을 방지하기 위함.
   { pathPrefix: '/reviews' },
 );
 
+//  다른 contract 를 모아서 하나의 contract 로 만들기.
 export const contract = c.router(
   {
     reviews: reviewsContract,
