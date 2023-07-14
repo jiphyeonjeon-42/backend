17fd5dacdff9a0640059a26efc9a335147de1a6d
scarf <greenscarf005@gmail.com>
2023-07-14 11:33:01 +0900

    style: 파일명 오타 수정

--- backend/src/routes/v2.ts
+++ backend/src/routes/v2.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { contract } from '@jiphyeonjeon-42/contracts';
 import { initServer } from '@ts-rest/express';
-import { reviewsControllerV2 } from '../reviews/controller/reviews.controlller.v2';
+import { reviewsControllerV2 } from '../reviews/controller/reviews.controller.v2';
 
 const s = initServer();
 export default s.router(contract, {
