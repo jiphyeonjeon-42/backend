a13d79d867894c98f1b4b8d7eee04925d227730e
scarf <greenscarf005@gmail.com>
2023-07-23 13:01:23 +0900

    feat: like 스키마
    Co-authored-by: Ji-Hyuck, Min <jhMin95@users.noreply.github.com>
    Co-authored-by: yena <nyj001012@users.noreply.github.com>

--- contracts/src/index.ts
+++ contracts/src/index.ts
@@ -1,13 +1,16 @@
 import { initContract } from '@ts-rest/core';
 import { reviewsContract } from './reviews';
+import { likesContract } from './likes';
 
 export * from './reviews';
+export * from './shared';
 
 const c = initContract();
 
 //  다른 contract 를 모아서 하나의 contract 로 만들기.
 export const contract = c.router(
   {
+    likes: likesContract,
     reviews: reviewsContract,
   },
   {
