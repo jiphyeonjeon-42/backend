26c79e23ad6742d56ea084c314b90a6696ba6759
scarf <greenscarf005@gmail.com>
2023-07-24 13:01:59 +0900

    refactor: inferRoute.ts 제거

--- backend/src/v2/inferRoute.ts
+++
@@ -1,9 +0,0 @@
-import type {
-  AppRoute,
-  ServerInferRequest,
-  ServerInferResponses,
-} from '@ts-rest/core';
-
-type ServerInferInput<T extends AppRoute> = {input: {req: ServerInferRequest<T>} }
-export type ServerInferRoute<T extends AppRoute> =
-  (input: ServerInferInput<T>) => Promise<ServerInferResponses<T>>
