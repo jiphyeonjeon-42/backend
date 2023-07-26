0356e35fa5f789c2d8095b042e488861236ddb74
yena <50291995+nyj001012@users.noreply.github.com>
2023-07-26 15:45:59 +0900

    feat(tag): 태그로 도서 검색한 결과의 View Entity 추가
    태그로 도서 검색한 결과의 View Entity 추가

---
+++ backend/src/entity/entities/VSearchBookByTag.ts
@@ -0,0 +1,68 @@
+import { DataSource, ViewColumn, ViewEntity } from 'typeorm';
+import Category from '~/entity/entities/Category';
+import Book from '~/entity/entities/Book';
+import BookInfo from './BookInfo';
+import SuperTag from './SuperTag';
+import SubTag from './SubTag';
+
+@ViewEntity('v_search_book_by_tag', {
+  expression: (Data: DataSource) => Data.createQueryBuilder()
+    .distinctOn(['bi.id'])
+    .select('bi.id', 'id')
+    .addSelect('bi.title', 'title')
+    .addSelect('bi.author', 'author')
+    .addSelect('bi.isbn', 'isbn')
+    .addSelect('bi.image', 'image')
+    .addSelect('bi.publishedAt', 'publishedAt')
+    .addSelect('bi.createdAt', 'createdAt')
+    .addSelect('bi.updatedAt', 'updatedAt')
+    .addSelect('c.name', 'category')
+    .addSelect('sp.content', 'superTagContent')
+    .addSelect('sb.content', 'subTagContent')
+    .addSelect((subQuery) => subQuery
+      .select('COUNT(l.id)')
+      .from('book', 'b')
+      .leftJoin('lending', 'l', 'l.bookId = b.id')
+      .innerJoin('book_info', 'bi2', 'bi2.id = b.infoId')
+      .where('bi.id = bi.id'), 'lendingCnt')
+    .from(BookInfo, 'bi')
+    .innerJoin(Category, 'c', 'c.id = bi.categoryId')
+    .innerJoin(SuperTag, 'sp', 'sp.bookInfoId = bi.id')
+    .leftJoin(SubTag, 'sb', 'sb.superTagId = sp.id'),
+})
+export class VSearchBookByTag {
+  @ViewColumn()
+    id: number;
+
+  @ViewColumn()
+    title: string;
+
+  @ViewColumn()
+    isbn: number;
+
+  @ViewColumn()
+    image: string;
+
+  @ViewColumn()
+    publishedAt: string;
+
+  @ViewColumn()
+    createdAt: string;
+
+  @ViewColumn()
+    updatedAt: string;
+
+  @ViewColumn()
+    category: string;
+
+  @ViewColumn()
+    superTagContent: string;
+
+  @ViewColumn()
+    subTagContent: string;
+
+  @ViewColumn()
+    lendingCnt: number;
+}
+
+export default VSearchBookByTag;
