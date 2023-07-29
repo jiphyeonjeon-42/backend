6bc0872879dbb372ee635b86f03a4b593305c4c4
yena <50291995+nyj001012@users.noreply.github.com>
2023-07-29 17:18:34 +0900

    feat: histories 라우터에 추가
    histories 라우터에 추가

--- backend/src/v2/routes.ts
+++ backend/src/v2/routes.ts
@@ -3,8 +3,10 @@ import { contract } from '@jiphyeonjeon-42/contracts';
 import { initServer } from '@ts-rest/express';
 
 import { reviews } from './reviews/impl';
+import { histories } from './histories/impl';
 
 const s = initServer();
 export default s.router(contract, {
   reviews,
+  histories
 });
