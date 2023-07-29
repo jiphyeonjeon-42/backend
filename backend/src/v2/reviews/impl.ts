922b425fa4618bae7f56c75c7088317511ae85e9
scarf <greenscarf005@gmail.com>
2023-07-29 18:50:00 +0900

    refactor: 컨트롤러 DI를 `controller/impl.ts`로 이동

--- backend/src/v2/reviews/impl.ts
+++ backend/src/v2/reviews/impl.ts
@@ -6,28 +6,14 @@ import authValidate from '~/v1/auth/auth.validate';
 import jipDataSource from '~/app-data-source';
 import BookInfo from '~/entity/entities/BookInfo';
 import Reviews from '~/entity/entities/Reviews';
-import {
-  mkDeleteReviews,
-  mkPatchReviews,
-  mkPostReviews,
-  mkPutReviews,
-} from './controller';
-import { ReviewsService as ReviewService } from './service';
+import { implReviewController } from './controller/impl';
 import { implReviewService } from './service/impl';
-
-const implReviewController = (service: ReviewService) => ({
-  post: mkPostReviews(service),
-  patch: mkPatchReviews(service),
-  put: mkPutReviews(service),
-  delete: mkDeleteReviews(service),
-});
 
 const service = implReviewService({
   reviews: jipDataSource.getRepository(Reviews),
   bookInfo: jipDataSource.getRepository(BookInfo),
 });
-
-export const handler = implReviewController(service);
+const handler = implReviewController(service);
 
 const s = initServer();
 export const reviews = s.router(contract.reviews, {
