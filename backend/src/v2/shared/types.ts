99b5742c5ff49feda409a58b249812e415e7d31b
yena <50291995+nyj001012@users.noreply.github.com>
2023-07-29 17:25:52 +0900

    feat: meta 스키마 추가
    meta 스키마 추가

--- backend/src/v2/shared/types.ts
+++ backend/src/v2/shared/types.ts
@@ -1,4 +1,14 @@
 import { AppRoute } from '@ts-rest/core';
 import { AppRouteOptions } from '@ts-rest/express';
+import { z } from 'zod';
 
 export type HandlerFor<T extends AppRoute> = AppRouteOptions<T>['handler'];
+
+export type Meta = z.infer<typeof meta>;
+export const meta = z.object({
+  totalItems: z.number().nonnegative(),
+  itemCount: z.number().nonnegative(),
+  itemsPerPage: z.number().nonnegative(),
+  totalPages: z.number().nonnegative(),
+  currentPage: z.number().nonnegative(),
+});
