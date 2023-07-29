6c7ad8e048464f9edceeae6bc737d33ecc5b46ad
scarf <greenscarf005@gmail.com>
2023-07-29 19:06:07 +0900

    refactor: reviews `impl.ts`을 역할에 따라 분리 (#602)
    * refactor: controller.ts를 `controller/` 디렉터리로 이동
    
    * refactor: 서비스 DI를 `service/impl.ts`로 이동
    
    * refactor: 컨트롤러 DI를 `controller/impl.ts`로 이동

--- backend/src/v2/reviews/impl.ts
+++ backend/src/v2/reviews/impl.ts
@@ -1,43 +1,18 @@
 import { contract } from '@jiphyeonjeon-42/contracts';
 import { initServer } from '@ts-rest/express';
-import jipDataSource from '~/app-data-source';
-import BookInfo from '~/entity/entities/BookInfo';
-import Reviews from '~/entity/entities/Reviews';
 import { roleSet } from '~/v1/auth/auth.type';
 import authValidate from '~/v1/auth/auth.validate';
 
-import { Repository } from 'typeorm';
-import { mkDeleteReviews, mkPatchReviews, mkPostReviews, mkPutReviews } from './controller';
-import {
-  ReviewsService as ReviewService,
-  mkCreateReview,
-  mkRemoveReview,
-  mkToggleReviewVisibility,
-  mkUpdateReview,
-} from './service';
-
-const implReviewService = (repos: {
-  reviews: Repository<Reviews>;
-  bookInfo: Repository<BookInfo>;
-}) => ({
-  createReview: mkCreateReview(repos),
-  removeReview: mkRemoveReview(repos),
-  updateReview: mkUpdateReview(repos),
-  toggleReviewVisibility: mkToggleReviewVisibility(repos),
-});
-
-const implReviewController = (service: ReviewService) => ({
-  post: mkPostReviews(service),
-  patch: mkPatchReviews(service),
-  put: mkPutReviews(service),
-  delete: mkDeleteReviews(service),
-});
+import jipDataSource from '~/app-data-source';
+import BookInfo from '~/entity/entities/BookInfo';
+import Reviews from '~/entity/entities/Reviews';
+import { implReviewController } from './controller/impl';
+import { implReviewService } from './service/impl';
 
 const service = implReviewService({
   reviews: jipDataSource.getRepository(Reviews),
   bookInfo: jipDataSource.getRepository(BookInfo),
 });
-
 const handler = implReviewController(service);
 
 const s = initServer();
