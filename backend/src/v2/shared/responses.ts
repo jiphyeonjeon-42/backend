3153397ade9babeaee0aadfd90f07e34549e7cb6
yena <50291995+nyj001012@users.noreply.github.com>
2023-07-29 17:25:35 +0900

    feat: 권한 없음 에러 오브젝트 추가
    권한 없음 에러 오브젝트 추가

--- backend/src/v2/shared/responses.ts
+++ backend/src/v2/shared/responses.ts
@@ -1,4 +1,4 @@
-import { bookInfoNotFoundSchema, reviewNotFoundSchema } from '@jiphyeonjeon-42/contracts';
+import { bookInfoNotFoundSchema, reviewNotFoundSchema, unauthorizedSchema } from '@jiphyeonjeon-42/contracts';
 import { z } from 'zod';
 
 export const reviewNotFound = {
@@ -15,4 +15,12 @@ export const bookInfoNotFound = {
     code: 'BOOK_INFO_NOT_FOUND',
     description: '검색한 책이 존재하지 않습니다.',
   } as z.infer<typeof bookInfoNotFoundSchema>,
+} as const;
+
+export const unauthorized = {
+  status: 401,
+  body: {
+    code: 'UNAUTHORIZED',
+    description: '권한이 없습니다.',
+  } as z.infer<typeof unauthorizedSchema>,
 } as const;
