37080d6802a7b0bc3a273ba4eb638126afd2cf94
yena <50291995+nyj001012@users.noreply.github.com>
2023-08-01 17:12:21 +0900

    feat(histories): histories service 추가
    - 전체 대출 기록 조회하는 service 추가
    - 나의 대출 기록 조회하는 service 추가

--- backend/src/v2/histories/service/service.ts
+++ backend/src/v2/histories/service/service.ts
@@ -1,41 +1,46 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { match } from 'ts-pattern';
 
-import { Like, type Repository } from 'typeorm';
+import { FindOperator, Like, type Repository } from 'typeorm';
 import VHistories from '~/entity/entities/VHistories';
 
 import { UnauthorizedError } from '~/v2/shared/errors';
 import User from '~/entity/entities/User';
-import type { HistoriesService } from '.';
 import { Meta } from '~/v2/shared';
+import type { HistoriesService } from '.';
 
 // HistoriesService
 
-type Repos = { historiesRepo: Repository<VHistories>, userRepo: Repository<User> };
+type Repos = { historiesRepo: Repository<VHistories> };
 
 type MkSearchHistories = (
   repos: Repos
-) => HistoriesService['search'];
+) => HistoriesService['searchAllHistories'];
+
+type whereCondition = {
+  login: FindOperator<string>,
+  title: FindOperator<string>,
+} | [{
+  login: FindOperator<string>,
+  title: FindOperator<string>,
+}];
 
 export const mkSearchHistories: MkSearchHistories = (histories) => async ({
-  query, who, page, limit, type,
-}, { userId, userRole }) => {
-  if (who === 'all' && userRole !== 2) {
-    return (new UnauthorizedError());
-  }
-  let filterQuery: any = {};
-  if (who === 'my') {
-    const user = await histories.userRepo.find({ where: { id: userId } });
-    filterQuery.login = user[0].nickname;
-  } else if (type === 'user') {
+  query, type, page, limit,
+}) => {
+  let filterQuery: whereCondition = {
+    login: Like('%%'),
+    title: Like('%%'),
+  };
+  if (type === 'user') {
     filterQuery.login = Like(`%${query}%`);
   } else if (type === 'title') {
     filterQuery.title = Like(`%${query}%`);
   } else {
-    filterQuery = [
-      { login: Like(`%${query}%`) },
-      { title: Like(`%${query}%`) },
-    ];
+    filterQuery = [{
+      login: Like(`%${query}%`),
+      title: Like(`%${query}%`),
+    }];
   }
   const [items, count] = await histories.historiesRepo.findAndCount({
     where: filterQuery,
@@ -49,5 +54,5 @@ export const mkSearchHistories: MkSearchHistories = (histories) => async ({
     totalPages: Math.ceil(count / limit),
     currentPage: page + 1,
   };
-  return { items, meta };
+  return [items, meta];
 };
