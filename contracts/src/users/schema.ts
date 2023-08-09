557c5ba2cbcf547428c19ee0437fcf97b6da0176
yena <50291995+nyj001012@users.noreply.github.com>
2023-08-09 14:28:47 +0900

    fix(users): nullable()을 optional()로 변경
    스키마에서 nullable()을 optional()로 변경

--- contracts/src/users/schema.ts
+++ contracts/src/users/schema.ts
@@ -2,10 +2,10 @@ import { mkErrorMessageSchema, positiveInt } from '../shared';
 import { z } from '../zodWithOpenapi';
 
 export const searchUserSchema = z.object({
-  nicknameOrEmail: z.string().nullable().describe('검색할 유저의 nickname or email'),
-  page: positiveInt.nullable().default(0).describe('페이지'),
-  limit: positiveInt.nullable().default(10).describe('한 페이지에 들어올 검색결과 수'),
-  id: positiveInt.nullable().describe('검색할 유저의 id'),
+  nicknameOrEmail: z.string().optional().describe('검색할 유저의 nickname or email'),
+  page: positiveInt.optional().default(0).describe('페이지'),
+  limit: positiveInt.optional().default(10).describe('한 페이지에 들어올 검색결과 수'),
+  id: positiveInt.optional().describe('검색할 유저의 id'),
 });
 
 const reservationSchema = z.object({
