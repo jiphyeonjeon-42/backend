51f770da8433f7a0fa9dee7594faf2ecc529521a
yena <50291995+nyj001012@users.noreply.github.com>
2023-08-09 14:52:22 +0900

    feat(users): 유저 생성 contract 추가
    - 유저 생성 contract 추가
    - pathprefix 추가

--- contracts/src/users/index.ts
+++ contracts/src/users/index.ts
@@ -3,22 +3,38 @@ import { badRequestSchema, serverErrorSchema } from '../shared';
 import {
   searchUserSchema,
   searchUserResponseSchema,
+  createUserSchema,
+  createUserResponseSchema,
 } from './schema';
 
 export * from './schema';
 
 const c = initContract();
 
-export const usersContract = c.router({
-  get: {
-    method: 'GET',
-    path: '/search',
-    query: searchUserSchema,
-    description: '유저 정보를 검색해 온다. query가 null이면 모든 유저를 검색한다.',
-    responses: {
-      200: searchUserResponseSchema,
-      400: badRequestSchema,
-      500: serverErrorSchema,
+export const usersContract = c.router(
+  {
+    get: {
+      method: 'GET',
+      path: '/search',
+      query: searchUserSchema,
+      description: '유저 정보를 검색해 온다. query가 null이면 모든 유저를 검색한다.',
+      responses: {
+        200: searchUserResponseSchema,
+        400: badRequestSchema,
+        500: serverErrorSchema,
+      },
+    },
+    post: {
+      method: 'POST',
+      path: '/create',
+      body: createUserSchema,
+      description: '유저를 생성한다.',
+      responses: {
+        201: createUserResponseSchema,
+        400: badRequestSchema,
+        500: serverErrorSchema,
+      },
     },
   },
-});
+  { pathPrefix: '/users' },
+);
