0ac11b48c5b09f6312c63bae7203a4011fe61a26
yena <50291995+nyj001012@users.noreply.github.com>
2023-08-09 14:57:18 +0900

    feat(users): patch user contract 추가
    user를 업데이트하는 patch 메서드 contract에 추가

--- contracts/src/users/index.ts
+++ contracts/src/users/index.ts
@@ -1,5 +1,5 @@
 import { initContract } from '@ts-rest/core';
-import { badRequestSchema, serverErrorSchema } from '../shared';
+import { badRequestSchema, positiveInt, serverErrorSchema } from '../shared';
 import {
   searchUserSchema,
   searchUserResponseSchema,
@@ -16,8 +16,8 @@ export const usersContract = c.router(
     get: {
       method: 'GET',
       path: '/search',
-      query: searchUserSchema,
       description: '유저 정보를 검색해 온다. query가 null이면 모든 유저를 검색한다.',
+      query: searchUserSchema,
       responses: {
         200: searchUserResponseSchema,
         400: badRequestSchema,
@@ -27,10 +27,22 @@ export const usersContract = c.router(
     post: {
       method: 'POST',
       path: '/create',
-      body: createUserSchema,
       description: '유저를 생성한다.',
+      body: createUserSchema,
       responses: {
         201: createUserResponseSchema,
+        400: badRequestSchema,
+        500: serverErrorSchema,
+      },
+    },
+    patch: {
+      method: 'PATCH',
+      path: 'update/:id',
+      description: '유저 정보를 변경한다.',
+      pathParams: userIdSchema,
+      body: updateUserSchema,
+      responses: {
+        200: updateUserResponseSchema,
         400: badRequestSchema,
         500: serverErrorSchema,
       },
