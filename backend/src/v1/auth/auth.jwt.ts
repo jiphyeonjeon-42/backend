8699b51fec7a6100d80e2aab3f64e9bf0644beac
scarf <greenscarf005@gmail.com>
2023-08-15 09:53:31 +0900

    fix: 로그아웃시 `access_token` 삭제 (#674)

--- backend/src/v1/auth/auth.jwt.ts
+++ backend/src/v1/auth/auth.jwt.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
-import { jwtOption } from '~/config';
+import { cookieOptions, jwtOption } from '~/config';
 import { User } from '../DTO/users.model';
 
 /**
@@ -26,19 +26,12 @@ export const issueJwt = (user: User) => {
  *
  * issueJwt 함수를 이용해 JWT를 생성하고, 토큰을 클라이언트 Cookie에 저장한다.
  * 설정값 설명
- *      httpOnly : 브라우저에서만 쿠키를 사용할 수 있게 설정
- *      secure : https 에서만 사용할 수 있도록 설정
- *      sameSite : 같은 도메인의에서만 쿠키를 사용할 수 있는 'strict' 값 설정
  *      expires: 밀리세컨드 값으로 설정해야하고, 1000 * 60 * 480 = 8시간으로 설정
  */
 export const saveJwt = async (req: Request, res: Response, user: User) : Promise<void> => {
   const token = issueJwt(user);
   res.cookie('access_token', token, {
-    httpOnly: true,
-    secure: jwtOption.secure, // ANCHOR https 연결시에는 true로 설정해주어야함.
-    sameSite: 'lax',
-    path: '/',
-    domain: jwtOption.domain,
+    ...cookieOptions,
     expires: new Date(new Date().getTime() + 1000 * 60 * 480),
   });
 };
