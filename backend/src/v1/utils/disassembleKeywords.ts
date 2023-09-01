0aa50e7a4e185f7822ab97c79b91b27bf44b2e88
Jiwon-Woo <jwoo@student.42seoul.kr>
2023-09-02 02:04:43 +0900

    feat: bookInfoSearchKeyword 리포지토리 및 create 함수 추가

--- backend/src/v1/utils/disassembleKeywords.ts
+++ backend/src/v1/utils/disassembleKeywords.ts
@@ -1,10 +1,14 @@
 import hangul from 'hangul-js';
 
-export const disassembleHangul = (original: string) =>
-  hangul.d(original).join('');
+export const disassembleHangul = (original: string | undefined) => {
+  if (!original) return '';
+  return hangul.d(original).join('');
+};
 
-export const extractHangulInitials = (original: string) =>
-  hangul
+export const extractHangulInitials = (original: string | undefined) => {
+  if (!original) return '';
+  return hangul
     .d(original, true)
     .map((letter) => letter[0])
     .join('');
+};
