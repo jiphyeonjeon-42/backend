b2d558e6b7456cfe1d767fb0edb01ba7fe56c21b
Jiwon-Woo <jwoo@student.42seoul.kr>
2023-09-01 20:20:54 +0900

    feat: 검색시 검색어를 저장하는 함수 추가

--- backend/src/v1/search-keywords/searchKeywords.service.ts
+++ backend/src/v1/search-keywords/searchKeywords.service.ts
@@ -1,4 +1,6 @@
-import { PopularSearchKeyword } from './searchKeywords.type';
+import { makeExecuteQuery, pool } from '~/mysql';
+import { logger } from '~/logger';
+import { PopularSearchKeyword, SearchKeyword } from './searchKeywords.type';
 import * as searchKeywordRepository from './searchKeywords.repository';
 
 const LEAST_SEARCH_COUNT = 5;
@@ -7,7 +9,10 @@ let lastPopular: string[] = [];
 
 const updateLastPopular = (items: string[]) => {
   lastPopular = [...items];
-  console.log(`(${new Date().toLocaleString()}) Popular Search Keywords `, lastPopular);
+  logger.debug(
+    `(${new Date().toLocaleString()}) Popular Search Keywords `,
+    lastPopular,
+  );
 };
 
 export const getPopularSearchKeywords = async () => {
@@ -40,4 +45,59 @@ export const renewLastPopular = async () => {
     POPULAR_RANKING_LIMIT,
   );
   updateLastPopular(popularKeywords.map((item) => item.keyword));
+};
+
+export const createSearchKeywordLog = async (
+  keyword: string,
+  disassemble: string,
+  initials: string,
+) => {
+  if (!keyword) return;
+
+  const connection = await pool.getConnection();
+  const transactionExecuteQuery = makeExecuteQuery(connection);
+
+  try {
+    await connection.beginTransaction();
+    const [searchKeyword]: [SearchKeyword | undefined] = await transactionExecuteQuery(
+      `
+      SELECT id, keyword
+      FROM search_keywords
+      WHERE keyword = ?
+      `,
+      [keyword],
+    );
+    console.log(searchKeyword);
+
+    let searchKeywordId = searchKeyword?.id;
+    if (!searchKeyword) {
+      const { insertId }: { insertId: number } = await transactionExecuteQuery(
+        `
+        INSERT INTO search_keywords
+        (keyword, disassembled_keyword, initial_consonants)
+        VALUES (?, ?, ?)
+        `,
+        [keyword, disassemble, initials],
+      );
+      searchKeywordId = insertId;
+    }
+
+    await transactionExecuteQuery(
+      `
+      INSERT INTO search_logs
+      (search_keyword_id)
+      VALUES (?)
+      `,
+      [searchKeywordId],
+    );
+
+    await connection.commit();
+  } catch (error) {
+    await connection.rollback();
+    if (error instanceof Error) {
+      throw error;
+    }
+  } finally {
+    connection.release();
+  }
 };
